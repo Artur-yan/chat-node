3025df40acf0d91de3dd9b2787865903baae5246
redshiftstudio <accounts@rdshft.io>
2023-03-29 20:08:17 +0100

    formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,20 +3,20 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
-      colors: {
-        'primary': {
-          100: '#F3F9FF',
-          200: '#E6F3FF',
-          300: '#CCE7FF',
-          400: '#99CFFF',
-          500: '#66B8FF',
-          600: '#4DA3FF',
-          700: '#338EFF',
-          800: '#1A79FF',
-          900: '#0064FF'
-        }
-      }
-    }
+			colors: {
+				primary: {
+					100: '#F3F9FF',
+					200: '#E6F3FF',
+					300: '#CCE7FF',
+					400: '#99CFFF',
+					500: '#66B8FF',
+					600: '#4DA3FF',
+					700: '#338EFF',
+					800: '#1A79FF',
+					900: '#0064FF'
+				}
+			}
+		}
 	},
 	plugins: []
 };
