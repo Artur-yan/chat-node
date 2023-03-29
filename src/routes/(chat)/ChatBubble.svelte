57d85d3960de70c6e6b1c6e02972fa62a42b5836
redshiftstudio <accounts@rdshft.io>
2023-03-29 15:38:16 +0100

    add color customizer

--- src/routes/(chat)/ChatBubble.svelte
+++ src/routes/(chat)/ChatBubble.svelte
@@ -1,14 +1,14 @@
 <script>
-    export let message = ""
-    export let side = "left"
+	export let message = '';
+	export let side = 'left';
 
-    let classes = "bg-indigo-500 text-white"
+    let classes = '';
 
-    if (side == 'right') {
-        classes = "self-end bg-blue-300"
-    }
+	if (side == 'right') {
+		classes = 'self-end';
+	}
 </script>
 
-<div class="p-4 rounded-lg max-w-2xl {classes}">
-    {message}
+<div class="p-4 rounded-lg max-w-2xl text-white {classes}" style="background-color: var({side == 'left' ? '--gptBubble' : '--userBubble'})">
+	{message}
 </div>
