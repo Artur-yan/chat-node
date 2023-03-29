26626757f4b3313e16449cb74daab4284472667a
redshiftstudio <accounts@rdshft.io>
2023-03-29 15:07:09 +0100

    autofocus chat input

--- src/routes/(chat)/ChatInput.svelte
+++ src/routes/(chat)/ChatInput.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <form class="absolute bottom-2 left-2 right-2" on:submit>
-    <input type="text" placeholder="type your message" bind:value={input} class="self-bottom w-full rounded-lg p-4" />
+    <input type="text" placeholder="type your message" bind:value={input} class="self-bottom w-full rounded-lg p-4" autofocus />
 </form>
--- src/routes/(chat)/ChatWindow.svelte
+++ src/routes/(chat)/ChatWindow.svelte
@@ -1,4 +1,4 @@
 
-<section class="relative p-2 rounded-md bg-slate-200 h-full flex flex-col items-start">
+<section class="relative p-2 rounded-lg bg-slate-200 h-full flex flex-col items-start">
     <slot />
 </section>
