311b74bfda26c99d3582ae86bca704745360e6d2
redshiftstudio <accounts@rdshft.io>
2023-04-15 13:33:57 +0200

    security / user auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,14 @@
 import { auth } from '$lib/server/lucia';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
+
+	if(event.url.pathname.startsWith('/account')){
+		if(!event.locals.auth){
+			 throw redirect(303, '/login');
+		}
+	}
+	
 	return await resolve(event);
 };
