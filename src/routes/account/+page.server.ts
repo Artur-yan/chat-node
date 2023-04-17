b4836d4c45ce3631fbd4e8c866c7957b1021819b
redshiftstudio <accounts@rdshft.io>
2023-04-17 19:08:48 +0200

    account plan serverside init

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -1,5 +1,25 @@
-import { redirect } from '@sveltejs/kit';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-export const load = async () => {
-	throw redirect(302, '/account/chatbots');
+export const load = async ({locals}) => {
+	const session = await locals.auth.validate();
+	if(!session) return;
+
+	try {
+		const plan = await prisma.authUser.findUnique({
+			where: {
+				id: session.userId
+			},
+			select: {
+				current_plan: true
+			}
+		});
+
+		return {session, plan};
+
+	} catch (err) {
+		console.error(err);
+	}
+
+
 };
