97d95b1ace1b1a47bf9bf710d0fb749bd1b07e49
redshiftstudio <accounts@rdshft.io>
2023-04-17 22:09:44 +0200

    semantic change

--- src/lib/components/TrainingStatus.svelte
+++ src/lib/components/TrainingStatus.svelte
@@ -3,12 +3,12 @@
 
 	export let trainingMessage: string;
 	export let visible = false;
-	export let training = true;
+	export let isTraining = true;
 </script>
 
 <div class:hidden={!visible} class="toast toast-start items-center gap-2">
 	<div class="alert alert-info">
-		{#if training}
+		{#if isTraining}
 			<Icon icon="line-md:uploading-loop" width="24" />
 		{:else}
 			<Icon icon="line-md:confirm-circle-twotone" width="24" />
