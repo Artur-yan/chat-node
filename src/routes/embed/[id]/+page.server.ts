c47fa3196b4e20617b9e314423978a3df198e1e4
redshiftstudio <accounts@rdshft.io>
2023-04-17 22:10:41 +0200

    enable domain filtering of frame ancestor

--- src/routes/embed/[id]/+page.server.ts
+++ src/routes/embed/[id]/+page.server.ts
@@ -4,14 +4,23 @@ import { error } from '@sveltejs/kit';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, setHeaders }) => {
+	
 	try {
 		const bot = await prisma.bots.findUniqueOrThrow({
 			where: {
 				id: params.id
 			}
 		});
-		return { bot };
+		// Set the Content-Security-Policy header to allow the embed to be loaded only on the whitelisted domains
+		if(bot.settings.public) {
+			setHeaders({
+				'Content-Security-Policy': `frame-ancestors 'self' ${bot.settings.allowedUrls.join(' ')};`
+			})
+			return { bot };
+		} else {
+			throw error(400, 'This bot is not public');
+		}
 	} catch (err) {
 		if (err.code == 'P2025') {
 			throw error(400, 'Bot not found');
