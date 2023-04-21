a1082ecd76d8761e4cfc13475f7862a6c3b3805b
redshiftstudio <accounts@rdshft.io>
2023-04-21 17:35:15 +0200

    make chat full height

--- src/routes/account/chatbots/[id]/+page.svelte
+++ src/routes/account/chatbots/[id]/+page.svelte
@@ -2,8 +2,12 @@
 	export let data;
 
 	let iframeEmbedCode = `<iframe src="/embed/${data.model.id}" width="100%" height="100%" style="border: none;"></iframe>`;
+
+	let top
+
+	$: console.log(top)
 </script>
 
-<div class="container h-full">
+<div class="container h-[calc(100vh_-_10em)]">
 	{@html iframeEmbedCode}
 </div>
