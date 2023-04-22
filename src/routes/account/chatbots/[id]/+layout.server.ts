77d440c7065a4accc3abd13a6bf5f8bd4e9ad68d
redshiftstudio <accounts@rdshft.io>
2023-04-22 19:28:46 +0200

    prevent viewing if not the owner, show same error regardless if valid bot_id

--- src/routes/account/chatbots/[id]/+layout.server.ts
+++ src/routes/account/chatbots/[id]/+layout.server.ts
@@ -1,12 +1,24 @@
 import { PrismaClient } from '@prisma/client';
+import { error } from '@sveltejs/kit';
+
 const prisma = new PrismaClient();
 
-export const load = async ({ params }) => {
+export const load = async ({ locals, params }) => {
     const model = await prisma.bots.findUnique({
         where: {
             id: params.id
         }
     });
 
-    return { model };
+    const user = await locals.auth.validateUser();
+
+    console.log(user)
+
+    if(user.session && user.session.userId === model.user_id) {
+        return { model };
+    } else {
+        throw error( 403,
+           { message: 'You are not authorized to view this page.'}
+        )
+    }
 };    
