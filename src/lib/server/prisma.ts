5aa9e68003e32f8a710648abd5a9884b1e52764f
redshiftstudio <accounts@rdshft.io>
2023-04-22 19:26:49 +0200

    cleanup

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,12 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { env } from '$env/dynamic/private';
-
-// const prisma = global.__prisma || new PrismaClient()
-
-// if (env.NODE_ENV === "development") {
-// 	global.__prisma = prisma
-// export {prisma}
-// }
 
 const prismaClient = new PrismaClient();
 
