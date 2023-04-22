3fe6213929f3478da92dab38dc0b04f1a472a259
redshiftstudio <accounts@rdshft.io>
2023-04-22 19:28:01 +0200

    add redirect for non-user

--- src/routes/account/+layout.server.ts
+++ src/routes/account/+layout.server.ts
@@ -1,7 +1,13 @@
 import type { LayoutServerLoad } from './settings/plan/$typesypes';
+import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const user = await locals.auth.validateUser();
 
-	return { user };
+	if(!user) {
+		throw redirect ( 302, '/' );
+	} else {
+		return { user };
+	}
+
 };
