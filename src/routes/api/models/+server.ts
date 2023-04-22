b272109596038b445f422ea10500385ccf5d3897
redshiftstudio <accounts@rdshft.io>
2023-04-22 17:38:39 +0200

    cleanup

--- src/routes/api/models/+server.ts
+++ src/routes/api/models/+server.ts
@@ -1,22 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-export const POST = async ({ request, locals }) => {
+export const POST = async ({ locals, request }) => {
+	const session = await locals.auth.validate();
 	const { id, data_source_type, name, settings } = await request.json();
 
-	const session = await locals.auth.validate();
-
-	await prisma.bots.create({
-		data: {
-			id,
-			user_id: session.userId,
-			data_source_type,
-			name,
-			settings
-		}
-	});
-
-	return new Response();
+	try{
+		await prisma.bots.create({
+			data: {
+				id,
+				user_id: session.userId,
+				data_source_type,
+				name,
+				settings
+			}
+		});
+	} catch (err) {
+		console.error(err);
+	}
+	return new Response(
+		JSON.stringify({ status: 200 })
+	);
 };
 
 export const PATCH = async ({ request, locals }) => {
