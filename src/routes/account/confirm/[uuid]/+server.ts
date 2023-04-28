e9390df9d1afc028a0ce804c5f4604a91d90d02a
redshiftstudio <accounts@rdshft.io>
2023-04-28 17:40:00 +0200

    prep for account settings implementation

--- src/routes/account/confirm/[uuid]/+server.ts
+++ src/routes/account/confirm/[uuid]/+server.ts
@@ -1,17 +1,28 @@
 import type { RequestHandler } from './$types';
-
-
 import { prismaClient } from '$lib/server/prisma';
 import { redirect } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ params, url }) => {
+    if(url.searchParams.get('update')) {
+        await prismaClient.authUser.update({
+            where: {
+                verification_uuid: params.uuid
+            },
+            data: {
+                email: user,
+                new_email: null,
+                verification_uuid: null
+            }
+        });
+        throw redirect(302, `/account/chatbots`);
+    }
     await prismaClient.authUser.update({
         where: {
             verification_uuid: params.uuid
         },
         data: {
-            status: 'active'
-        }
+            status: 'active',
+        },
     })
     throw redirect(302, `/account/chatbots`);
 };
