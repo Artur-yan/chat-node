1f700651ca5a55e05200969c2dae84d3af75be53
redshiftstudio <accounts@rdshft.io>
2023-05-02 16:38:52 +0200

    iframe policy

--- src/routes/embed/[id]/+page.server.ts
+++ src/routes/embed/[id]/+page.server.ts
@@ -16,12 +16,10 @@ export const load: PageServerLoad = async ({ params, setHeaders }) => {
 			setHeaders({
 				'Content-Security-Policy': `frame-ancestors 'self' ${bot.settings.allowedUrls.join(' ')};`
 			});
+			return { bot };
 		} else {
-			setHeaders({
-				'Content-Security-Policy': `frame-ancestors 'self'`
-			});
+			throw error(403, 'Embed is not public');
 		}
-		return { bot };
 	} catch (err) {
 		if (err.code == 'P2025') {
 			throw error(400, 'Bot not found');
