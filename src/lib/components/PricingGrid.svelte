bd0435dc8b986d323aa165cb4aa75ab12316ba0f
redshiftstudio <accounts@rdshft.io>
2023-05-08 00:30:57 +0200

    move all plan updating login into component

--- src/lib/components/PricingGrid.svelte
+++ src/lib/components/PricingGrid.svelte
@@ -1,14 +1,63 @@
-<h2 class="text-center !text-white">Pricing</h2>
-<div class="my-10 grid md:grid-cols-3 gap-y-20">
+<script lang="ts">
+	import { alert } from '$lib/stores.js';
+	import { goto } from '$app/navigation';
+
+	export let currentPlan: number | null;
+
+	let busyChangingPlan = false;
+
+	$:console.log(currentPlan);
+
+	const updatePlan = async (newPlan: number) => {
+		try {
+			busyChangingPlan = true;
+			const res = await fetch('/api/account/plan', {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(newPlan)
+			});
+			const data = await res.json();
+			console.log(data);
+			if (data.url) {
+				goto(data.url);
+			} else if (data.status == 'success') {
+				currentPlan = newPlan;
+				$alert = 'Plan updated successfully';
+			}
+		} catch (err) {
+			console.error(err);
+		} finally {
+			busyChangingPlan = false;
+		}
+	};
+</script>
+
+<div class="container my-10 grid md:grid-cols-3 gap-y-20">
 	<div class="rounded-l-xl p-8 border border-secondary rounded-r-none my-4 bg-base-200">
 		<div class="flex justify-between mb-8">
 			<h2>Free</h2>
-			<div class="btn btn-outline btn-secondary">Sign up</div>
+			{#if currentPlan !== null}
+				<button
+					on:click={() => updatePlan(0)}
+					class="btn btn-outline btn-secondary"
+					class:loading={busyChangingPlan}
+					disabled={currentPlan === 0}>{currentPlan === 0 ? 'Current plan' : 'Change plan'}</button
+				>
+			{:else}
+				<a href="/register" class="btn btn-outline btn-secondary">Sign up</a>
+			{/if}
 		</div>
 		<ul class="included">
 			<li>30 messages / month</li>
 			<li>1 Chatbot</li>
 			<li>Embed on unlimited websites</li>
+		<!-- <ul class="excluded">
+			<li>2000 messages / month</li>
+			<li>5 Chatbots</li>
+			<li>Embed on your website using an iframe or JS plugin</li>
+		</ul> -->
 		</ul>
 	</div>
 	<div class="border border-secondary p-8 -mx-[1px] rounded-2xl bg-base-100">
@@ -17,18 +66,23 @@
 				<h2>Basic</h2>
 				<h3 class="text-lg font-bold">30€<span class="text-xs opacity-70 ml-1 align-top">/ mo.</span></h3>
 			</div>
-			<div class="btn btn-outline btn-secondary">Sign up</div>
+			{#if currentPlan !== null}
+				<button
+					on:click={() => updatePlan(1)}
+					class="btn btn-outline btn-secondary"
+					class:loading={busyChangingPlan}
+					disabled={currentPlan === 1}>{currentPlan === 1 ? 'Current plan' : 'Change plan'}</button
+				>
+			{:else}
+				<a href="/register" class="btn btn-outline btn-secondary">Sign up</a>
+			{/if}
 		</div>
 		<ul class="included">
 			<li>2,000 messages / month</li>
 			<li>Up to 10 Chatbots</li>
 			<li>Embed on unlimited websites</li>
 		</ul>
-		<!-- <ul class="excluded">
-			<li>2000 messages / month</li>
-			<li>5 Chatbots</li>
-			<li>Embed on your website using an iframe or JS plugin</li>
-		</ul> -->
+
 	</div>
 	<div class="p-8 border border-secondary rounded-r-xl my-4 bg-base-200">
 		<div class="flex justify-between mb-8">
@@ -36,7 +90,16 @@
 				<h2>Pro</h2>
 				<h3 class="text-lg font-bold">50€<span class="text-xs opacity-70 ml-1 align-top">/ mo.</span></h3>
 			</div>
-			<div class="btn btn-outline btn-secondary">Sign up</div>
+			{#if currentPlan !== null}
+				<button
+					on:click={() => updatePlan(2)}
+					class="btn btn-outline btn-secondary"
+					class:loading={busyChangingPlan}
+					disabled={currentPlan === 2}>{currentPlan === 2 ? 'Current plan' : 'Change plan'}</button
+				>
+			{:else}
+				<a href="/register" class="btn btn-outline btn-secondary">Sign up</a>
+			{/if}
 		</div>
 		<ul class="included">
 			<li>5,000 chat responses / month</li>
@@ -54,7 +117,16 @@
 					</div>
 				</div>
 				<div>
-					<div class="btn btn-outline btn-secondary">Sign Up</div>
+					{#if currentPlan !== null}
+						<button
+							on:click={() => updatePlan(4)}
+							class="btn btn-outline btn-secondary"
+							class:loading={busyChangingPlan}
+							disabled={currentPlan === 4}>{currentPlan === 4 ? 'Current plan' : 'Change plan'}</button
+						>
+					{:else}
+						<a href="/register" class="btn btn-outline btn-secondary">Sign up</a>
+					{/if}
 					<div class="btn btn-outline btn-secondary">Contact Us</div>
 				</div>
 			</div>
@@ -94,5 +166,9 @@
 
 	.card-actions {
 		@apply mt-8;
+	}
+
+	.btn:disabled{
+		@apply btn-secondary !text-neutral opacity-80
 	}
 </style>
--- src/routes/account/settings/plan/+page.svelte
+++ src/routes/account/settings/plan/+page.svelte
@@ -1,109 +1,11 @@
 <script lang="ts">
 	import { alert } from '$lib/stores.js';
 	import { goto } from '$app/navigation';
+	import PricingGrid from '$lib/components/PricingGrid.svelte';
 
 	export let data;
 
-	let plan = data.subscription.plan;
-
-	const updatePlan = async (newPlan: number) => {
-		const res = await fetch('/api/account/plan', {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(newPlan)
-		});
-		const data = await res.json();
-		if (data.url) {
-			goto(data.url);
-		} else if (data.status == 'success') {
-			plan = newPlan;
-			$alert = 'Plan updated successfully';
-		}
-	};
+	let currentPlan = data.subscription.plan;
 </script>
 
-<section>
-	<div class="container my-10 grid md:grid-cols-3 gap-8">
-		<label>
-			<div class="card">
-				<h2 class="card-title p-4 bg-cyan-900 rounded-lg">FREE</h2>
-				<div class="card-body">
-					<ul>
-						<li>30 chat responses / month</li>
-						<li>Up to 3 Chatbots</li>
-						<li>Embed on your website using an iframe or JS plugin</li>
-					</ul>
-					<div class="card-actions justify-end">
-						<button
-							on:click={() => updatePlan(0)}
-							class="btn btn-outline btn-secondary w-full"
-							disabled={plan == 0}>{plan == 0 ? 'Current plan' : 'Change plan'}</button
-						>
-					</div>
-				</div>
-			</div>
-		</label>
-		<label>
-			<div class="card">
-				<h2 class="card-title p-4 bg-primary rounded-lg text-neutral">PLUS</h2>
-				<div class="card-body">
-					<ul>
-						<li>30 chat responses / month</li>
-						<li>Up to 3 Chatbots</li>
-						<li>Embed on your website using an iframe or JS plugin</li>
-					</ul>
-					<div class="card-actions justify-end">
-						<button
-							on:click={() => updatePlan(1)}
-							class="btn btn-outline btn-secondary w-full"
-							disabled={plan == 1}>{plan == 1 ? 'Current plan' : 'Change plan'}</button
-						>
-					</div>
-				</div>
-			</div>
-		</label>
-		<label>
-			<div class="card">
-				<h2 class="card-title p-4 bg-secondary rounded-lg text-neutral">PRO</h2>
-				<div class="card-body">
-					<ul>
-						<li>30 chat responses / month</li>
-						<li>Up to 3 Chatbots</li>
-						<li>Embed on your website using an iframe or JS plugin</li>
-					</ul>
-					<div class="card-actions justify-end">
-						<button
-							on:click={() => updatePlan(2)}
-							class="btn btn-outline btn-secondary w-full"
-							disabled={plan == 2}>{plan == 2 ? 'Current Plan' : 'Change plan'}</button
-						>
-					</div>
-				</div>
-			</div>
-		</label>
-	</div>
-</section>
-
-<style lang="postcss">
-	.card {
-		@apply card-compact bg-neutral shadow-xl;
-	}
-
-	input:checked + .card .btn {
-		@apply btn-primary;
-	}
-
-	ul {
-		@apply divide-y divide-base-100 py-4;
-	}
-
-	li {
-		@apply py-3;
-	}
-
-	.card .btn[disabled] {
-		@apply btn-secondary opacity-75;
-	}
-</style>
+<PricingGrid {currentPlan} />
