b4073855ef057aaa048852509c36c042ad720de7
redshiftstudio <accounts@rdshft.io>
2023-05-07 22:32:36 +0200

    only include night theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	plugins: [require('daisyui')]
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ["night"],
+	  },
 };
