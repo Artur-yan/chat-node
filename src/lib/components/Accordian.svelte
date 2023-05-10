8ea7e636a1ffe38f21867a45dd6166023fa87a93
redshiftstudio <accounts@rdshft.io>
2023-05-10 13:03:41 +0200

    add accordians to settings

--- src/lib/components/Accordian.svelte
+++ src/lib/components/Accordian.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	let open = false;
+	export let open = false;
 </script>
 
 <div class="border border-primary/50 bg-base-300 rounded-xl hover:shadow-xl hover:bg-base-200 transition-all" class:open>
 	<button
 		on:click={() => (open = !open)}
+		type="button"
 		class="flex text-lg justify-between text-left items-center w-full p-5 font-bold"
 	>
 		<slot name="title" />
@@ -33,7 +34,7 @@
 		grid-template-rows: 0fr;
 	}
 	.open .body {
-		@apply max-h-[5em] p-4 opacity-100;
+		@apply p-4 opacity-100;
 		grid-template-rows: 1fr;
 	}
 </style>
--- src/lib/components/ModelSettings.svelte
+++ src/lib/components/ModelSettings.svelte
@@ -3,6 +3,7 @@
 	import Icon from '@iconify/svelte';
 	import { goto } from '$app/navigation';
 	import { alert } from '$lib/stores';
+	import Accordian from '$lib/components/Accordian.svelte';
 
 	export let id: string;
 	export let name: string;
@@ -29,7 +30,6 @@
 
 <div class="@container">
 	<form on:submit={handleSubmit} class="grid gap-10 @[240px]:grid-cols-2 pb-32">
-		<div class="space-y-3">
 			<div>
 				<label for="name" class="label">
 					<span class="label-text">Name</span>
@@ -41,82 +41,98 @@
 					placeholder="Untitled"
 				/>
 			</div>
-
-			<div>
-				<label for="greeting" class="label">
-					<span class="label-text">Greeting</span>
-				</label>
-				<input type="text" bind:value={settings.greeting} class="input w-full" />
-			</div>
-			<div>
-				<label for="support-message" class="label">
-					<span class="label-text">Support Message</span>
-				</label>
-				<input type="text" bind:value={settings.supportMessage} class="input w-full" />
-				<p class="text-sm m-1">
-					This is the message that will appear to the user if the chatbot cannot come up with a
-					confident answer. You may want to include an email address or link to a contact page here.
-				</p>
-			</div>
-			<div>
-				<label for="system-prompt" class="label">
-					<span class="label-text">System Prompt</span>
-				</label>
-				<textarea
-					bind:value={settings.systemPrompt}
-					class="textarea textarea-bordered w-full leading-normal"
-					name="prompt"
-					rows="5"
-				/>
-			</div>
-			<div>
-				<label for="support-message" class="label">
-					<span class="label-text">User Prompt</span>
-				</label>
-				<textarea
-					bind:value={settings.userPrompt}
-					class="textarea textarea-bordered w-full"
-					name="user-prompt"
-				/>
-			</div>
-		</div>
-		<div>
-			<div class="form-control">
-				<label class="label cursor-pointer flex justify-start gap-4 self-start">
-					<span class="label-text">Private</span>
-					<input
-						type="checkbox"
-						class="toggle toggle-warning input-success"
-						bind:checked={settings.public}
-					/>
-					<span class="label-text">Public</span>
-				</label>
-			</div>
-			{#if settings.public}
-				<div class="space-y-4">
-					<p class="text-xs">
-						Add specific urls you would like to allow this chatbot to be displayed on.
-					</p>
-					{#each settings.allowedUrls as url, i}
-						<div class="input-group">
-							<input
-								name="url-{i}"
-								bind:value={settings.allowedUrls[i]}
-								class="input w-full"
-								placeholder="https://example.com"
-								autofocus
-							/>
-							<button class="btn text-error/75" on:click={() => removeUrl(i)}
-								><Icon icon="mdi:minus-circle-outline" width="16" /></button
-							>
-						</div>
-					{/each}
-					<button class="btn btn-xs btn-ghost btn-primary" type="button" on:click={() => addUrl('')}
-						><Icon icon="mdi:plus-circle-outline" width="16" class="mr-2" /> Add URL</button
-					>
+			<Accordian open={true}>
+				<div slot="title">
+					Messages
 				</div>
-			{/if}
-		</div>
+				<div>
+					<label for="greeting" class="label">
+						<span class="label-text">Greeting</span>
+					</label>
+					<input type="text" bind:value={settings.greeting} class="input w-full" />
+				</div>
+				<div>
+					<label for="support-message" class="label">
+						<span class="label-text">Support Message</span>
+					</label>
+					<input type="text" bind:value={settings.supportMessage} class="input w-full" />
+					<p class="text-sm m-1">
+						This is the message that will appear to the user if the chatbot cannot come up with a
+						confident answer. You may want to include an email address or link to a contact page here.
+					</p>
+				</div>
+			</Accordian>
+			<Accordian open={true}>
+				<div slot="title">
+					Prompts
+				</div>
+
+				<div>
+					<div>
+						<label for="system-prompt" class="label">
+							<span class="label-text">System Prompt</span>
+						</label>
+						<textarea
+							bind:value={settings.systemPrompt}
+							class="textarea textarea-bordered w-full leading-normal"
+							name="prompt"
+							rows="5"
+						/>
+					</div>
+					<div>
+						<label for="user-prompt" class="label">
+							<span class="label-text">User Prompt</span>
+						</label>
+						<textarea
+							bind:value={settings.userPrompt}
+							class="textarea textarea-bordered w-full"
+							name="user-prompt"
+						/>
+					</div>
+				</div>
+			</Accordian>
+		<Accordian open={true}>
+			<div slot="title">
+				Publishing
+			</div>
+			<div>
+				<div class="form-control">
+					<label class="label cursor-pointer flex justify-start gap-4 self-start">
+						<span class="label-text">Private</span>
+						<input
+							type="checkbox"
+							class="toggle toggle-warning input-success"
+							bind:checked={settings.public}
+						/>
+						<span class="label-text">Public</span>
+					</label>
+				</div>
+				{#if settings.public}
+					<div class="space-y-4">
+						<p class="text-xs">
+							Add specific urls you would like to allow this chatbot to be displayed on.
+						</p>
+						{#each settings.allowedUrls as url, i}
+							<div class="input-group">
+								<input
+									name="url-{i}"
+									bind:value={settings.allowedUrls[i]}
+									class="input w-full"
+									placeholder="https://example.com"
+									autofocus
+								/>
+								<button class="btn text-error/75" on:click={() => removeUrl(i)}
+									><Icon icon="mdi:minus-circle-outline" width="16" /></button
+								>
+							</div>
+						{/each}
+						<button class="btn btn-xs btn-ghost btn-primary" type="button" on:click={() => addUrl('')}
+							><Icon icon="mdi:plus-circle-outline" width="16" class="mr-2" /> Add URL</button
+						>
+					</div>
+				{/if}
+			</div>
+		</Accordian>
 		<div class="fixed bottom-0 left-0 right-0 bg-base-300 p-2 flex items-center justify-between">
 			<button
 				class="btn btn-outline btn-success md:w-40"
