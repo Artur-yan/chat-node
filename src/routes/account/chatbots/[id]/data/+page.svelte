88f7e0813cbe41613bd273bcfca7b83e41668e1b
redshiftstudio <accounts@rdshft.io>
2023-05-10 11:08:23 +0200

    add chat to page

--- src/routes/account/chatbots/[id]/data/+page.svelte
+++ src/routes/account/chatbots/[id]/data/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	export let data
 	import AddModelData from "$lib/components/AddModelData.svelte";
+	import Chat from "$lib/components/Chat.svelte";
 </script>
 
-<div class="container">
-
-	<AddModelData modelId={data.model.id} userId={data.user.user.userId} sessionId={data.user.session.sessionId} subscription={data.subscription} />
+<div class="container grid grid-cols-2 gap-10">
+	<div>
+		<AddModelData modelId={data.model.id} userId={data.user.user.userId} sessionId={data.user.session.sessionId} subscription={data.subscription} />
+	</div>
+	<div>
+		<Chat modelId={data.model.id} />
+	</div>
 </div>
