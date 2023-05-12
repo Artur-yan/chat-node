7211495e04810e834de07a8cc90299db94f439cd
redshiftstudio <accounts@rdshft.io>
2023-05-12 16:57:08 +0200

    accessibility

--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -165,7 +165,7 @@
 				style="background-color: var(--inputBG); color: var(--inputText);"
 				{disabled}
 			/>
-			<button class="btn btn-square" type="submit">
+			<button class="btn btn-square" type="submit" name="Send">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
 					><path
 						fill="none"
