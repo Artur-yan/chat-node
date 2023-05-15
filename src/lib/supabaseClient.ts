2fbf00266c12fc7f465a2fc6223293472702b213
redshiftstudio <accounts@rdshft.io>
2023-05-15 20:22:36 +0200

    move to server side

--- src/lib/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-// src/supabaseClient.ts
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
-
-const supabase: SupabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY);
-
-export default supabase;
