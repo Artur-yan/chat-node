26f2ccd0319eee306aac034c40ea959afc247676
redshiftstudio <accounts@rdshft.io>
2023-05-18 15:36:20 +0200

    improve full height scaling, make app shell flex

--- src/routes/account/chatbots/[id]/+page.svelte
+++ src/routes/account/chatbots/[id]/+page.svelte
@@ -7,6 +7,6 @@
 	<title>{data.model.name} | ChatNode</title>
 </svelte:head>
 
-<div class="container h-[calc(100vh_-_10em)]">
+<div class="container h-full">
 	<Chat modelId={data.model.id} status={data.model.status} settings={data.model.settings} />
 </div>
