7461a0f5f79dd1464ccbf52f20eb51c05ef10da4
redshiftstudio <accounts@rdshft.io>
2023-05-25 12:00:51 +0200

    fix background color appearing around chatwindow

--- src/routes/embed/[id]/+layout.svelte
+++ src/routes/embed/[id]/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
+	import { redirect } from '@sveltejs/kit';
 	import './embed.postcss';
 </script>
 
 <slot />
+
+<div>
+	<style>
+		body{
+			background-color: transparent !important;
+		}
+	</style>
+</div>
