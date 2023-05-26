6fd5d62c8a9bedb6447a4009ec75ecee529d4042
Sacha Dumay <dumaysacha@gmail.com>
2023-05-26 22:30:04 +0100

    slack integration sacha

--- src/routes/account/chatbots/[id]/integrations/+page.svelte
+++ src/routes/account/chatbots/[id]/integrations/+page.svelte
@@ -1,21 +1,25 @@
 <script>
-	import { goto } from '$app/navigation';
-    import { PUBLIC_CHAT_API_URL, PUBLIC_SITE_URL } from '$env/static/public';
+    import { PUBLIC_CHAT_API_URL, PUBLIC_SITE_URL } from "$env/static/public";
 
     export let data;
 
     let loading = false
     const handleSubmit = async () => {
-        loading = true
-        const res = await fetch (`${PUBLIC_CHAT_API_URL}/slack/install`, {
-            method: 'GET',
-            
-            headers: {
-                'Content-Type': 'application/json',
-                'Set-Cookie': `user_id=${data.model.user_id}; bot_id=${data.model.id} Domain=${PUBLIC_SITE_URL}`,
-            }
-        })
-        console.log(res)
+        // Redirect to a different website
+        //window.location.href = `${PUBLIC_CHAT_API_URL}/slack/install`; I don't know why sometime it appends it, sometimes not to normal domain
+
+        window.location.href = `https://api.dev.chatnode.ai/slack/install`;
+
+        // loading = true
+        // const res = await fetch (`${PUBLIC_CHAT_API_URL}/slack/install`, {
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Set-Cookie': `user_id=${data.model.user_id}; bot_id=${data.model.id}`
+        //     }
+        // })
+        // console.log(res)
+
         // if(res.status === 200){
         //     goto(`/account/chatbots/${data.model.id}/integrations/slack`, {
 
