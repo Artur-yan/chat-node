f96aaf59b5adf60c8a55fd4132a96568d28db190
redshiftstudio <accounts@rdshft.io>
2023-05-26 14:57:26 +0200

    add loading indicator and console log res

--- src/routes/account/chatbots/[id]/integrations/+page.svelte
+++ src/routes/account/chatbots/[id]/integrations/+page.svelte
@@ -3,17 +3,22 @@
 
     export let data;
 
+    let loading = false
     const handleSubmit = async () => {
-        await fetch (`${PUBLIC_CHAT_API_URL}/stripe/install`, {
+        loading = true
+        const res = await fetch (`${PUBLIC_CHAT_API_URL}/stripe/install`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'Set-Cookie': `user_id=${data.model.user_id}; bot_id=${data.model.id} Domain=${PUBLIC_SITE_URL}`,
             }
         })
+        data = await res.json()
+        console.log(data)
+        loading = false
     }
 </script>
 
 <div class="container py-20 text-center">
-    <button class="btn btn-primary" on:click={() => handleSubmit()}>Authorize Stripe</button>
+    <button class="btn btn-primary" on:click={() => handleSubmit()} class:loading>Authorize Stripe</button>
 </div>
