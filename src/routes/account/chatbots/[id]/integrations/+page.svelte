365f6b27c2eb10934e88cb6083061700fc703f00
redshiftstudio <accounts@rdshft.io>
2023-05-26 15:19:26 +0200

    log response

--- src/routes/account/chatbots/[id]/integrations/+page.svelte
+++ src/routes/account/chatbots/[id]/integrations/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { goto } from '$app/navigation';
     import { PUBLIC_CHAT_API_URL, PUBLIC_SITE_URL } from '$env/static/public';
 
     export let data;
@@ -8,14 +9,18 @@
         loading = true
         const res = await fetch (`${PUBLIC_CHAT_API_URL}/slack/install/${data.model.user_id}/${data.model.id}`, {
             method: 'GET',
+            
             headers: {
                 'Content-Type': 'application/json',
                 'Set-Cookie': `user_id=${data.model.user_id}; bot_id=${data.model.id} Domain=${PUBLIC_SITE_URL}`,
             }
         })
-        data = await res.json()
-        console.log(data)
-        loading = false
+        console.log(res)
+        // if(res.status === 200){
+        //     goto(`/account/chatbots/${data.model.id}/integrations/slack`, {
+
+        //     })
+        // }
     }
 </script>
 
