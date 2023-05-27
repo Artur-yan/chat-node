06fd0d34cb42ed277ad5db4bc851f37aa3b42bdf
redshiftstudio <accounts@rdshft.io>
2023-05-27 12:59:23 +0200

    bake in domain

--- src/routes/account/chatbots/[id]/integrations/+page.server.ts
+++ src/routes/account/chatbots/[id]/integrations/+page.server.ts
@@ -3,8 +3,8 @@ import { PUBLIC_CHAT_API_URL } from '$env/static/public';
 export const load = async ({ cookies, locals, params }) => {
     const user = await locals.auth.validateUser();
 
-	const user_id = cookies.set('user_id', user.user?.userId, { path: '/', domain: PUBLIC_CHAT_API_URL });
-	const bot_id = cookies.set('bot_id', params.id, { path: '/', domain: PUBLIC_CHAT_API_URL });
+	const user_id = cookies.set('user_id', user.user?.userId, { path: '/', domain: '.chatnode.ai' });
+	const bot_id = cookies.set('bot_id', params.id, { path: '/', domain: '.chatnode.ai' });
 
 	return {
 		user_id,
