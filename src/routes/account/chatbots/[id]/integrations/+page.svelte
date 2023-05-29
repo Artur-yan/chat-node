9f702c714c3c01d83c7ac4ab1be3692f96b036ce
redshiftstudio <accounts@rdshft.io>
2023-05-29 18:33:44 +0200

    check if slack integration exists

--- src/routes/account/chatbots/[id]/integrations/+page.svelte
+++ src/routes/account/chatbots/[id]/integrations/+page.svelte
@@ -3,8 +3,22 @@
 
     export let data;
 
+    console.log(data)
+
 </script>
 
-<div class="container py-20 text-center">
-    <a class="btn btn-primary" href="{PUBLIC_CHAT_API_URL}/slack/install">Authorize Slack</a>
+<div class="container pb-10 grid sm:grid-cols-2 md:grid-cols-3">
+    <div class="card bg-base-300">
+        <div class="card-body">
+            <h2 class="card-title">Slack</h2>
+            <p class="text-base text-neutral-content">
+                Get responses from your custom trained chatbot directly in slack for you and your team.
+            </p>
+            {#if data.model.slack_bot_status === false}
+                <a class="btn btn-primary mt-10" href="{PUBLIC_CHAT_API_URL}/slack/install">Authorize Slack</a>
+            {:else}
+                <div class="badge badge-outline badge-success mt-10">Enabled</div>
+            {/if}
+        </div>
+    </div>
 </div>
