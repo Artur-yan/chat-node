065b2f6592f7492235193dc217b6975e0ec6a8ae
redshiftstudio <accounts@rdshft.io>
2023-05-29 17:32:39 +0200

    cleanup

--- src/routes/account/chatbots/[id]/integrations/+page.svelte
+++ src/routes/account/chatbots/[id]/integrations/+page.svelte
@@ -3,30 +3,6 @@
 
     export let data;
 
-    // let loading = false
-    // const handleSubmit = async () => {
-    //     // Redirect to a different website
-    //     //window.location.href = `${PUBLIC_CHAT_API_URL}/slack/install`; I don't know why sometime it appends it, sometimes not to normal domain
-
-    //     window.location.href = `https://api.dev.chatnode.ai/slack/install`;
-    //     goto('')
-
-    //     // loading = true
-    //     // const res = await fetch (`${PUBLIC_CHAT_API_URL}/slack/install`, {
-    //     //     method: 'GET',
-    //     //     headers: {
-    //     //         'Content-Type': 'application/json',
-    //     //         'Set-Cookie': `user_id=${data.model.user_id}; bot_id=${data.model.id}`
-    //     //     }
-    //     // })
-    //     // console.log(res)
-
-    //     // if(res.status === 200){
-    //     //     goto(`/account/chatbots/${data.model.id}/integrations/slack`, {
-
-    //     //     })
-    //     // }
-    // }
 </script>
 
 <div class="container py-20 text-center">
