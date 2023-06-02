eccf6b8fb45dae9af8d7a7ebed2515d2aba79d91
redshiftstudio <accounts@rdshft.io>
2023-06-02 18:15:42 +0200

    add feature

--- src/lib/components/Features.svelte
+++ src/lib/components/Features.svelte
@@ -6,13 +6,11 @@
     <div>
         <h2><Icon icon="grommet-icons:stripe" class="text-[#635BFF]" width={24} />Stripe integration</h2>
         
-            <p>With just a couple click you can connect any of your bots to any/all of your slack workspaces and get brilliant responses about your data without ever leavng your slack channel.</p>
+        <p>With just a couple click you can connect any of your bots to any/all of your slack workspaces and get brilliant responses about your data without ever leavng your slack channel.</p>
     </div>
-    <div>
-        
-
-            <h2><Icon icon="fluent:chat-12-regular" class="text-secondary" width={30} />Chatbot</h2>
-            <p>Stop fielding customer support emails repsonding to the same questions over and over. Let our attractive ai chatbot give intelligent automated responses to your audience.</p>
+    <div>       
+        <h2><Icon icon="fluent:chat-12-regular" class="text-secondary" width={30} />Chatbot</h2>
+        <p>Stop fielding customer support emails repsonding to the same questions over and over. Let our attractive ai chatbot give intelligent automated responses to your audience.</p>
     </div>
     <div>
         <h2><Icon icon="iconoir:code" class="text-accent" width={32} />Embed anywhere</h2>
@@ -25,6 +23,10 @@
     <div>
         <h2><Icon icon="ri:exchange-funds-fill" class="text-warning" width={32} />Hyperspeed iteration</h2>
         <p>Add and remove data, change your prompt, customize the colors, all without ever leaving the chatbot with our unique UI for superfast experimentation.</p>
+    </div>
+    <div>
+        <h2><Icon icon="basil:lock-solid" class="text-error" width={32} />Public/Private</h2>
+        <p>Keep your bot private for a personal edge, share it with the world, or deploy it for your team to sort through big data.</p>
     </div>
 
 </section>
