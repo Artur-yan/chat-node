bc820ae2effb2f9bee36e84f60f2019bc60bfc09
redshiftstudio <accounts@rdshft.io>
2023-06-03 16:45:32 +0200

    add testing of event with proxy

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -6,7 +6,15 @@
 	import Plausible from 'plausible-tracker';
 
 	onMount(async () => {
-		const { trackEvent } = Plausible({ domain: 'chatnode.ai' });
+		const { trackEvent } = Plausible({
+			domain: 'chatnode.ai',
+			apiHost: 'https://chatnode.ai/events'
+		});
+		if ($page.url.searchParams.get('plan-change') === 'test') {
+			trackEvent('Upgrade to Paid Test');
+			goto('/account/settings/plan');
+			$alert = 'Plan changed successfully!';
+		}
 		if ($page.url.searchParams.get('plan-change') === 'success') {
 			trackEvent('Upgrade to Paid');
 			goto('/account/settings/plan');
