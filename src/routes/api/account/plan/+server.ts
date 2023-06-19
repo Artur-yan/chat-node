86e2a7ad7a3d3bc14a516f4370ca81bfd7b50718
redshiftstudio <accounts@rdshft.io>
2023-06-19 18:54:14 +0200

    remove unused settings code

--- src/routes/api/account/plan/+server.ts
+++ src/routes/api/account/plan/+server.ts
@@ -1,8 +1,7 @@
 import { PUBLIC_CHAT_API_URL } from '$env/static/public';
-import { prismaClient } from '$lib/server/prisma.js';
 
 export const PUT = async ({ request, locals }) => {
-	const { newPlan, referralCode, settings } = await request.json();
+	const { newPlan, referralCode } = await request.json();
 	const session = await locals.auth.validate();
 
 	try {
@@ -12,23 +11,12 @@ export const PUT = async ({ request, locals }) => {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
-				user_id: session.userId,
+				user_id: session?.userId,
 				plan: newPlan,
 				referral: referralCode
 			})
 		});
 		const url = await res.json();
-		if (newPlan === 0) {
-			settings.gptVersion = '3.5';
-			// prismaClient.bots.updateMany({
-			// 	where: {
-			// 		user_id: session.userId
-			// 	},
-			// 	data: {
-			// 		settings
-			// 	}
-			// })
-		}
 		return new Response(JSON.stringify(url), { status: 200 });
 	} catch (err) {
 		console.error(err);
