1c4f800d3edcf5e3aee286903017c3b5bfb6935c
redshiftstudio <accounts@rdshft.io>
2023-06-22 14:23:36 +0200

    fix image on js chat button

--- static/embed.js
+++ static/embed.js
@@ -16,8 +16,8 @@ const html = `
         <iframe src="https://www.chatnode.ai/embed/${id}" width="100%" height="100%"></iframe>
     </div>
     <div role="button" style="outline: 0; border: none; position: absolute; bottom: 0; right: 0; width:64px; height: 64px;" onclick="toggleChat()">
-	<svg xmlns="http://www.w3.org/2000/svg" width="66" height="65" fill="none" xmlns:v="https://vecta.io/nano"><g clip-path="url(#B)" filter="url(#A)"><path d="M65 32c0 17.673-14.327 32-32 32S1 49.673 1 32 15.327 0 33 0s32 14.327 32 32z" fill="${color2}"/><path fill-rule="evenodd" d="M27 16c-5.523 0-10 4.477-10 10v12c0 5.523 4.477 10 10 10h19.5a2.5 2.5 0 0 0 2.5-2.5V26c0-5.523-4.477-10-10-10H27zm-.75 16a2 2 0 1 1-4 0 2 2 0 1 1 4 0zM35 32a2 2 0 1 1-4 0 2 2 0 1 1 4 0zm6.75 2a2 2 0 1 0 0-4 2 2 0 1 0 0 4z" fill="${color1}"/></g><defs><filter id="A" x=".5" y="0" width="65" height="65" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="A"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="B"/><feOffset dy="0.5"/><feGaussianBlur stdDeviation=".25"/><feComposite in2="B" operator="out"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/><feBlend in2="A"/><feBlend in="SourceGraphic"/></filter><clipPath id="B"><path fill="#fff" transform="translate(1)" d="M0 0h64v64H0z"/></clipPath></defs></svg>
-		</div>
+	<svg clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" version="1.1" viewBox="0 0 64 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g><path d="m0 32c0-17.673 14.327-32 32-32s32 14.327 32 32-14.327 32-32 32-32-14.327-32-32z" fill="#0e1729"/><path d="m26 16c-5.5228 0-10 4.4772-10 10v12c0 5.5228 4.4772 10 10 10h19.5c1.3807 0 2.5-1.1193 2.5-2.5v-19.5c0-5.5228-4.4772-10-10-10h-12zm-0.75 16c0 1.1046-0.8954 2-2 2s-2-0.8954-2-2 0.8954-2 2-2 2 0.8954 2 2zm8.75 0c0 1.1046-0.8954 2-2 2s-2-0.8954-2-2 0.8954-2 2-2 2 0.8954 2 2zm6.75 2c1.1046 0 2-0.8954 2-2s-0.8954-2-2-2-2 0.8954-2 2 0.8954 2 2 2z" fill="#3abff7" fill-rule="evenodd"/></g></svg>
+	</div>
 </div>
 `;
 document.addEventListener('DOMContentLoaded', () => {
