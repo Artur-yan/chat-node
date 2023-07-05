1d5ac5e51ef5baa34844865f86a60e4036e66ac0
redshiftstudio <accounts@rdshft.io>
2023-07-05 12:18:27 +0200

    new pricing grid design with enterprise+

--- src/lib/components/PricingGrid.svelte
+++ src/lib/components/PricingGrid.svelte
@@ -56,7 +56,19 @@
 
 	</div>
 
-	<div class="my-10 grid md:grid-cols-3 gap-y-8 lg:gap-y-20">
+	<div class="card border-neutral text-center">
+		<div class="card-body">
+			<h2 class="!text-xl mb-4 uppercase">All plans include</h2>
+			<ul class="included flex items-center gap-x-10 gap-y-4 justify-center flex-wrap">
+				<li>Add Multiple Data Sources</li>
+				<li>Embed on unlimited websites</li>
+				<li>Custom Colors</li>
+				<li>Chatbot, iframe and web address sharing</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="my-8 grid md:grid-cols-3 gap-y-8 lg:gap-y-20">
 		<div class="rounded-xl md:rounded-r-none p-8 border border-secondary md:my-4 bg-base-200">
 			<div class="flex justify-between mb-8">
 				<h2>Free</h2>
@@ -74,19 +86,17 @@
 					<a href="/account/settings/plan" class="btn btn-outline btn-secondary">Sign up</a>
 				{/if}
 			</div>
-			<ul class="included">
-				<li>30 messages/month</li>
-				<li>1 Chatbot</li>
+			<ul>
+				<li><span class="figure">30</span> messages/month</li>
+				<li><span class="figure">1</span> chatbot</li>
 				<li>
 					<div
 						class="tooltip border-b border-dotted border-white/30"
 						data-tip="approx. 400,000 characters"
 					>
-						100,000 tokens/bot
+					<span class="figure">100,000</span> tokens/bot
 					</div>
 				</li>
-				<li>Add Multiple Data Sources</li>
-				<li>Embed on unlimited websites</li>
 			</ul>
 		</div>
 		<div class="border border-secondary p-8 py-12 md:-mx-[1px] rounded-2xl bg-base-100 shadow-xl">
@@ -127,19 +137,17 @@
 					<a href="/account/settings/plan" class="btn btn-outline btn-secondary">Sign up</a>
 				{/if}
 			</div>
-			<ul class="included">
-				<li>2,000 messages/month</li>
-				<li>Up to 5 Chatbots</li>
+			<ul>
+				<li><span class="figure">2,000</span> messages/month</li>
+				<li><span class="figure">5</span> Chatbots</li>
 				<li>
 					<div
 						class="tooltip border-b border-dotted border-white/30"
 						data-tip="approx. 1,600,000 characters"
 					>
-						400,000 tokens/bot
+					<span class="figure">400,000</span> tokens/bot
 					</div>
 				</li>
-				<li>Add Multiple Data Sources</li>
-				<li>Embed on unlimited websites</li>
 			</ul>
 		</div>
 		<div class="p-8 border border-secondary rounded-xl md:rounded-l-none my-4 bg-base-200">
@@ -180,88 +188,142 @@
 					<a href="/account/settings/plan" class="btn btn-outline btn-secondary">Sign up</a>
 				{/if}
 			</div>
-			<ul class="included">
-				<li>5,000 messages/month</li>
-				<li>10 Chatbots</li>
+			<div class="grid lg:grid-cols-2">
+			<ul>
+				<li><span class="figure">5,000</span> messages/month</li>
+				<li><span class="figure">10</span> Chatbots</li>
 				<li>
 					<div
 						class="tooltip border-b border-dotted border-white/30"
 						data-tip="approx. 3,200,000 characters"
 					>
-						800,000 tokens/bot
+					<span class="figure">800,000</span> tokens/bot
 					</div>
 				</li>
-				<li>Add Multiple Data Sources</li>
-				<li>Embed on unlimited websites</li>
-				<ul class="extra">
-					<li>Add custom logo</li>
-				</ul>
 			</ul>
+			<ul class="extra">
+				<li>Add custom logo</li>
+				<li>Slack integration</li>
+			</ul>
+			</div>
 		</div>
-		<div class="col-span-full">
-			<div class="border border-secondary p-8 rounded-2xl max-w-6xl mx-auto bg-base-200">
-				<div class="flex justify-between">
-					<div class="flex items-center justify-between mb-8">
-						<div>
-							<h2>Enterprise</h2>
-							<h3 class="text-lg font-bold">
-								{#if billingTerm === 'monthly'}
-									<div>$99<span class="text-xs opacity-70 ml-1 align-top">/ mo.</span></div>
-								{:else}
-									<div>$990<span class="text-xs opacity-70 ml-1 align-top">/ yr.</span></div>
-								{/if}
-							</h3>
-						</div>
-					</div>
+	</div>
+	<div class="grid grid-cols-2 max-w-7xl mx-auto">
+		<div class="border border-secondary p-8 rounded-l-2xl border-r-0 bg-base-200">
+			<div class="flex justify-between">
+				<div class="flex items-center justify-between mb-8">
 					<div>
-						{#if currentPlan !== undefined}
-						{#if billingTerm == 'monthly'}
-						<button
-							on:click={() => updatePlan(3)}
-							class="btn btn-outline btn-secondary"
-							disabled={currentPlan === 3}
-						>
-							<span class={busyChangingPlan ? 'loading' : 'invisible'} />
-
-							{currentPlan === 3 ? 'Current plan' : 'Change plan'}</button
-						>
-					{:else}
-						<button
-							on:click={() => updatePlan(103)}
-							class="btn btn-outline btn-secondary"
-							disabled={currentPlan === 103}
-						>
-							<span class={busyChangingPlan ? 'loading' : 'invisible'} />
-
-							{currentPlan === 103 ? 'Current plan' : 'Change plan'}</button
-						>
-					{/if}
-						{:else}
-							<a href="/account/settings/plan" class="btn btn-outline btn-secondary">Sign up</a>
-						{/if}
+						<h2>Enterprise</h2>
+						<h3 class="text-lg font-bold">
+							{#if billingTerm === 'monthly'}
+								<div>$99<span class="text-xs opacity-70 ml-1 align-top">/ mo.</span></div>
+							{:else}
+								<div>$990<span class="text-xs opacity-70 ml-1 align-top">/ yr.</span></div>
+							{/if}
+						</h3>
 					</div>
 				</div>
 				<div>
-					<ul class="included grid grid-cols-2">
-						<li>10,000 messages/month</li>
-						<li>20 Chatbots</li>
-						<li>
-							<div
-								class="tooltip border-b border-dotted border-white/30"
-								data-tip="approx. 4,000,000 characters"
-							>
-								1,000,000 tokens/bot
-							</div>
-						</li>
-						<li>Add Multiple Data Sources</li>
-						<li>Embed on unlimited websites</li>
-						<li>Add custom logo</li>
-					</ul>
-					<ul class="extra">
-						<li>Access chat history</li>
-						<li><span class="">Get a customized plan for greater usage</span></li>
-					</ul>
+					{#if currentPlan !== undefined}
+					{#if billingTerm == 'monthly'}
+					<button
+						on:click={() => updatePlan(3)}
+						class="btn btn-outline btn-secondary"
+						disabled={currentPlan === 3}
+					>
+						<span class={busyChangingPlan ? 'loading' : 'invisible'} />
+
+						{currentPlan === 3 ? 'Current plan' : 'Change plan'}</button
+					>
+				{:else}
+					<button
+						on:click={() => updatePlan(103)}
+						class="btn btn-outline btn-secondary"
+						disabled={currentPlan === 103}
+					>
+						<span class={busyChangingPlan ? 'loading' : 'invisible'} />
+
+						{currentPlan === 103 ? 'Current plan' : 'Change plan'}</button
+					>
+				{/if}
+					{:else}
+						<a href="/account/settings/plan" class="btn btn-outline btn-secondary">Sign up</a>
+					{/if}
 				</div>
+			</div>
+			<div>
+				<ul>
+					<li><span class="figure">10,000</span> messages/month</li>
+					<li><span class="figure">20</span> Chatbots</li>
+					<li>
+						<div
+							class="tooltip border-b border-dotted border-white/30"
+							data-tip="approx. 4,000,000 characters"
+						>
+						<span class="figure">1,000,000</span> tokens/bot
+						</div>
+					</li>
+				</ul>
+				<ul class="extra">
+					<li>Access chat history</li>
+				</ul>
+			</div>
+		</div>
+		<div class="border border-secondary p-8 rounded-r-2xl bg-base-100">
+			<div class="flex justify-between">
+				<div class="flex items-center justify-between mb-8">
+					<div>
+						<h2>Enterprise <span class="text-white/80">+</span></h2>
+						<h3 class="text-lg font-bold">
+							{#if billingTerm === 'monthly'}
+								<div>$399<span class="text-xs opacity-70 ml-1 align-top">/ mo.</span></div>
+							{:else}
+								<div>$3990<span class="text-xs opacity-70 ml-1 align-top">/ yr.</span></div>
+							{/if}
+						</h3>
+					</div>
+				</div>
+				<div>
+					{#if currentPlan !== undefined}
+					{#if billingTerm == 'monthly'}
+					<button
+						on:click={() => updatePlan(4)}
+						class="btn btn-outline btn-secondary"
+						disabled={currentPlan === 4}
+					>
+						<span class={busyChangingPlan ? 'loading' : 'invisible'} />
+
+						{currentPlan === 3 ? 'Current plan' : 'Change plan'}</button
+					>
+				{:else}
+					<button
+						on:click={() => updatePlan(104)}
+						class="btn btn-outline btn-secondary"
+						disabled={currentPlan === 104}
+					>
+						<span class={busyChangingPlan ? 'loading' : 'invisible'} />
+
+						{currentPlan === 104 ? 'Current plan' : 'Change plan'}</button
+					>
+				{/if}
+					{:else}
+						<a href="/account/settings/plan" class="btn btn-outline btn-secondary">Sign up</a>
+					{/if}
+				</div>
+			</div>
+			<div>
+				<ul>
+					<li><span class="figure">40,000</span> messages/month</li>
+					<li><span class="figure">40</span> Chatbots</li>
+					<li>
+						<div
+							class="tooltip border-b border-dotted border-white/30"
+							data-tip="approx. 12,000,000 characters"
+						>
+						<span class="figure">3,000,000</span> tokens/bot
+						</div>
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
@@ -273,17 +335,24 @@
 	}
 
 	li {
-		@apply mb-2 relative pl-8;
+		@apply mb-2 relative;
 	}
 
 	.included li {
-		background: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="16" height="16" viewBox="0 0 24 24"%3E%3Cpath fill="%23818CF8" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83L9 20.42Z"%2F%3E%3C%2Fsvg%3E')
-			no-repeat left 2px;
+		background: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="20" height="20" viewBox="0 0 24 24"%3E%3Cpath fill="%23818CF8" d="m9 20.42l-6.21-6.21l2.83-2.83L9 14.77l9.88-9.89l2.83 2.83L9 20.42Z"%2F%3E%3C%2Fsvg%3E')
+			no-repeat left 3px;
+			@apply pl-8 text-lg;
 	}
 
 	.extra li {
-		background: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="18" height="18" viewBox="0 0 24 24"%3E%3Cpath fill="%23F470B4" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"%2F%3E%3C%2Fsvg%3E')
-			no-repeat left 2px;
+		background: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="15" height="15" viewBox="0 0 15 15"%3E%3Cpath fill="%23f470b4" d="M14 7H9V2H7v5H2v2h5v5h2V9h5V7z"%2F%3E%3C%2Fsvg%3E')
+			no-repeat 0 50%;
+			@apply pl-6;
+	}
+
+	.figure{
+		@apply text-secondary font-bold;
+
 	}
 
 	li::before {
