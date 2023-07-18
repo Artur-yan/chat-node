043a7cf31ec2f679ab898e275bde15638a67c599
redshiftstudio <accounts@rdshft.io>
2023-07-18 15:55:32 -0400

    styling

--- src/routes/account/chatbots/[id]/settings/+layout.svelte
+++ src/routes/account/chatbots/[id]/settings/+layout.svelte
@@ -36,21 +36,24 @@
 
 <div class="container grid lg:grid-cols-[12rem_auto_18rem] xl:lg:grid-cols-[12rem_auto_24rem] gap-4 py-4 h-full">
     <div class="h-full relative">
-                <ul class="sticky top-4 menu bg-base-200 rounded-box" role="navigation">
+        <div class="sticky top-4">
+
+            <ul class="menu bg-base-200 rounded-box space-y-1" role="navigation">
                 <li class="menu-title">Settings</li>
                 {#each links as link}
-                    <li><a href='/account/chatbots/{$page.params.id}/settings/{link.url}' class:active={link.url === currentPath}>{link.name}</a></li>
+                <li><a href='/account/chatbots/{$page.params.id}/settings/{link.url}' class:active={link.url === currentPath}>{link.name}</a></li>
                 {/each}
-              </ul>
+            </ul>
+            <button class="btn btn-outline btn-success my-4 w-full" type="submit" on:click={handleSave}>{$state == 'saving' ? 'Saving' : 'Save'}</button>
+        </div>
                 
-                  <button class="btn btn-outline btn-success my-4 w-full" type="submit" on:click={handleSave}>{$state == 'saving' ? 'Saving' : 'Save'}</button>
 
     </div>
     <div>
         <slot />
     </div>
 
-    <div class="h-full relative">
+    <div class="h-full min-h-[24rem] relative hidden sm:block">
 		<div class="sticky top-4 h-3/4 max-h-[64rem]">
 			<Chat modelId={data.model.id} settings={$currentBot.settings} trainingStatus={data.model.status} avatar={$currentBot.settings.avatarImg} />
 		</div>
