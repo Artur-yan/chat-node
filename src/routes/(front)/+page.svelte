b4ccd3de2efe025c15cfea38cc50b83db80e5833
redshiftstudio <accounts@rdshft.io>
2023-08-04 15:23:19 +0200

    improve arrow trig accuracy

--- src/routes/(front)/+page.svelte
+++ src/routes/(front)/+page.svelte
@@ -43,13 +43,12 @@
 	let arrowRotation: number;
 
 	const trig = () => {
-		const arrowY = arrow.offsetTop - scrollY;
-		const arrowX = arrow.offsetLeft;
-		const trigO = outerHeight - arrowY - 52;
-		const trigA = outerWidth - arrowX;
+		const arrowPosition = arrow.getBoundingClientRect();
+		const trigO = outerHeight - (arrowPosition.top + arrow.offsetHeight / 2) - 52;
+		const trigA = outerWidth - (arrowPosition.left + arrow.offsetWidth / 2) - 52;
 		const theta = Math.atan(trigO / trigA);
 
-		console.log(theta);
+		console.log(outerHeight);
 		arrowRotation = theta;
 	};
 
@@ -198,11 +197,23 @@
 			</div>
 		</div>
 
-		<div class="text-center flex flex-col px-6">
-			<div class="mockup-window bg-neutral shadow-lg p-4">
+		<div class="text-center flex flex-col px-6" onclick="toggleChat()">
+			<div class="mockup-window relative bg-neutral shadow-lg p-4 h-full">
 				<h3 class="font-bold mb-4">Popup Chat</h3>
+				<div
+					bind:this={arrow}
+					class="absolute w-12 left-1/4 top-1/2 flex items-center justify-center aspect-square text-4xl text-primary"
+					style="transform: rotate({arrowRotation}rad);"
+				>
+					&rarr;
+					<div
+						class="absolute top-0 left-0 radial-progress text-primary/50"
+						style="--size: 3rem; --value: 30; --thickness: 2px; transform: rotate({arrowRotation *
+							15}rad);"
+					/>
+				</div>
 
-				<div class="flex items-end justify-end">
+				<div class="absolute bottom-4 right-4">
 					<div class="max-w-xs">
 						<div class="p-2 bg-base-200 border-primary/50 rounded-lg border">
 							<div>
@@ -214,20 +225,11 @@
 								</div>
 							</div>
 						</div>
-						<div class="flex items-center justify-end mt-4">
-							<div
-								bind:this={arrow}
-								class="relative w-12 flex items-center justify-center aspect-square text-4xl text-primary"
-								style="transform: rotate({arrowRotation}rad);"
-							>
-								&rarr;
-								<div
-									class="absolute top-0 left-0 radial-progress text-primary/50"
-									style="--size: 3rem; --value: 30; --thickness: 2px; transform: rotate({arrowRotation *
-										15}rad);"
-								/>
+						<div class="flex items-center justify-end mt-2">
+							<div role="button" style="outline: 0; border: none; width:32px; height: 32px;">
+								<svg class="fill-primary" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" version="1.1" viewBox="0 0 64 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g><path d="m0 32c0-17.673 14.327-32 32-32s32 14.327 32 32-14.327 32-32 32-32-14.327-32-32z" class="fill-base-100"/><path d="m26 16c-5.5228 0-10 4.4772-10 10v12c0 5.5228 4.4772 10 10 10h19.5c1.3807 0 2.5-1.1193 2.5-2.5v-19.5c0-5.5228-4.4772-10-10-10h-12zm-0.75 16c0 1.1046-0.8954 2-2 2s-2-0.8954-2-2 0.8954-2 2-2 2 0.8954 2 2zm8.75 0c0 1.1046-0.8954 2-2 2s-2-0.8954-2-2 0.8954-2 2-2 2 0.8954 2 2zm6.75 2c1.1046 0 2-0.8954 2-2s-0.8954-2-2-2-2 0.8954-2 2 0.8954 2 2 2z" fill-rule="evenodd"/></g></svg>
+								</div>
 							</div>
-						</div>
 					</div>
 				</div>
 			</div>
@@ -273,7 +275,7 @@
 </section>
 
 <div class="py-20 bg-[url('/grid.png')] bg-[length:1vw] relative">
-	<div class="absolute inset-0 bg-gradient-to-b from-base-200 via-transparent to-base-200" />
+	<div class="absolute inset-0 bg-gradient-to-b from-base-200 via-transparent to-base-200 shadow-base-200" />
 	<div class="flex justify-center">
 		<h2 class="font-bold text-3xl relative py-4 px-4 w-[22vw] text-center text-secondary">
 			<div class="dot-corners">
