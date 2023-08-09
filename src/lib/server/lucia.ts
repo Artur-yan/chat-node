1c64152ac75c2666eb6e163302d033e8d9e22604
redshiftstudio <accounts@rdshft.io>
2023-08-09 21:00:26 +0100

    update all fix lucia bug

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -1,6 +1,6 @@
 import lucia from 'lucia-auth';
 import { sveltekit } from 'lucia-auth/middleware';
-import prisma from '@lucia-auth/adapter-prisma';
+import {prisma} from '@lucia-auth/adapter-prisma';
 import { dev } from '$app/environment';
 import { prismaClient } from '$lib/server/prisma';
 
