c3784410402fdb3b2a6fdef4528d197c1f44d931
redshiftstudio <accounts@rdshft.io>
2023-08-13 21:57:50 +0100

    add feature

--- src/lib/components/Features.svelte
+++ src/lib/components/Features.svelte
@@ -1,13 +1,16 @@
 <script>
-	import Icon from '@iconify/svelte';
-
 	let classesIcon = 'text-secondary inline-block';
 </script>
 
 <section class="text-center flex flex-col py-[5vw] shadow-inner">
 	<div class="flex items-end justify-center">
 		<div class="rounded-tl-md">
-			<h2><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13.5 6L10 18.5m-3.5-10L3 12l3.5 3.5m11-7L21 12l-3.5 3.5"/></svg>Embed anywhere</h2>
+			<h2><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path fill="currentColor" d="m20.58 19.37l-2.99-8.36c-.21-.55-.68-.89-1.22-.89s-1 .34-1.23.91l-2.98 8.34a.75.75 0 1 0 1.41.51l.62-1.73h4.35l.62 1.73c.11.31.4.5.71.5c.08 0 .17-.01.25-.04a.75.75 0 0 0 .45-.96Zm-5.84-2.73l1.64-4.59l1.64 4.59h-3.28Zm-2.55-8.79c-2.26 3.57-4.3 5.73-6.78 7.17a.746.746 0 0 1-1.02-.27a.738.738 0 0 1 .27-1.02c2.1-1.22 3.82-2.97 5.75-5.87H4.12c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h3.75V4.38c0-.41.34-.75.75-.75s.75.34.75.75v1.98h3.75c.41 0 .75.34.75.75s-.34.75-.75.75h-.94Zm.04 7.27c-.13 0-.26-.03-.38-.1c-.65-.38-1.28-.8-1.87-1.24a.75.75 0 0 1 .9-1.2c.54.41 1.13.79 1.73 1.14a.752.752 0 0 1-.38 1.4Z"/></svg>190 Languages</h2>
+			<div class="dot dot-bl" />
+			<div class="dot dot-tr" />
+		</div>
+		<div class="rounded-tl-md">
+			<h2><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13.5 6L10 18.5m-3.5-10L3 12l3.5 3.5m11-7L21 12l-3.5 3.5"/></svg>Embed anywhere</h2>
 			<div class="dot dot-bl" />
 			<div class="dot dot-tr" />
 		</div>
