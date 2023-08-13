d31eda1b3b5d87123b08f5866e478a540ab37add
redshiftstudio <accounts@rdshft.io>
2023-08-13 15:20:37 +0100

    add icons to share methods

--- src/routes/(front)/+page.svelte
+++ src/routes/(front)/+page.svelte
@@ -201,7 +201,11 @@
 
 	<div class="grid md:grid-cols-2 my-10">
 		<div class="p-6 text-center border-r border-b border-neutral">
-			<h3 class="font-bold mb-4 text-secondary">Embed</h3>
+			<h3 class="font-bold mb-4 text-primary text-xl flex items-center justify-center gap-2">
+				<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13.5 6L10 18.5m-3.5-10L3 12l3.5 3.5m11-7L21 12l-3.5 3.5"/></svg>
+Embed
+</h3>
+
 
 			<div class="mockup-window bg-neutral shadow-lg p-4 pb-8">
 				<div class="inline-block h-4 w-24 bg-base-100 rounded-full mx-auto mb-3" />
@@ -222,7 +226,9 @@
 		</div>
 
 		<div class="text-center flex flex-col p-6 border-b border-neutral" onclick="toggleChat()">
-			<h3 class="font-bold mb-4 text-secondary">Popup Chat</h3>
+			<h3 class="font-bold mb-4 text-primary text-xl flex items-center justify-center gap-2">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g transform="translate(24 0) scale(-1 1)"><g fill="none"><path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z"/><path fill="currentColor" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10H4a2 2 0 0 1-2-2v-8C2 6.477 6.477 2 12 2Zm0 2a8 8 0 0 0-8 8v8h8a8 8 0 1 0 0-16Zm0 10a1 1 0 0 1 .117 1.993L12 16H9a1 1 0 0 1-.117-1.993L9 14h3Zm3-4a1 1 0 1 1 0 2H9a1 1 0 1 1 0-2h6Z"/></g></g></svg>
+				Popup Chat</h3>
 			<div class="mockup-window relative bg-neutral shadow-lg p-4 h-full">
 				<div
 					bind:this={arrow}
@@ -281,7 +287,10 @@
 		</div>
 
 		<div class="p-6 border-r border-neutral">
-			<h3 class="font-bold mb-4 text-secondary text-center">URL</h3>
+			<h3 class="font-bold mb-4 text-primary text-xl flex items-center justify-center gap-2">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g transform="translate(24 0) scale(-1 1)"><g fill="none"><path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z"/><path fill="currentColor" d="M10.232 10.231a5 5 0 0 1 6.89-.172l.181.172l2.828 2.829a5 5 0 0 1-6.89 7.243l-.18-.172l-2.122-2.122a1 1 0 0 1 1.32-1.497l.094.083l2.122 2.122a3 3 0 0 0 4.377-4.1l-.135-.143l-2.828-2.829a3 3 0 0 0-4.243 0a1 1 0 0 1-1.414-1.414ZM3.868 3.867a5 5 0 0 1 6.89-.172l.181.172L13.06 5.99a1 1 0 0 1-1.32 1.497l-.094-.083l-2.121-2.121A3 3 0 0 0 5.147 9.38l.135.144l2.829 2.829a3 3 0 0 0 4.242 0a1 1 0 1 1 1.415 1.414a5 5 0 0 1-6.89.172l-.182-.172l-2.828-2.829a5 5 0 0 1 0-7.07Z"/></g></g></svg>
+				URL
+</h3>
 
 			<div class="mockup-browser bg-neutral h-full">
 				<div class="mockup-browser-toolbar">
@@ -301,7 +310,11 @@
 			  </div>
 		</div>
 		<div class="p-6">
-			<h3 class="font-bold mb-4 text-secondary text-center">Slack</h3>
+			<h3 class="font-bold mb-4 text-primary text-xl flex items-center justify-center gap-2">
+				<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="currentColor" d="M6 15a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2h2v2m1 0a2 2 0 0 1 2-2a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2a2 2 0 0 1-2-2v-5m2-8a2 2 0 0 1-2-2a2 2 0 0 1 2-2a2 2 0 0 1 2 2v2H9m0 1a2 2 0 0 1 2 2a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2a2 2 0 0 1 2-2h5m8 2a2 2 0 0 1 2-2a2 2 0 0 1 2 2a2 2 0 0 1-2 2h-2v-2m-1 0a2 2 0 0 1-2 2a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2a2 2 0 0 1 2 2v5m-2 8a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2v-2h2m0-1a2 2 0 0 1-2-2a2 2 0 0 1 2-2h5a2 2 0 0 1 2 2a2 2 0 0 1-2 2h-5Z"/></svg>
+
+				Slack
+</h3>
 
 			<div class="mockup-browser bg-neutral h-full">
 				<div class="mockup-browser-toolbar">
