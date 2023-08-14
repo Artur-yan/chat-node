e59403794006a0757c8bc16a46b22839ca49b4f6
redshiftstudio <accounts@rdshft.io>
2023-08-14 11:48:04 +0100

    unsaved changes dialog

--- src/routes/account/chatbots/[id]/settings/+layout.svelte
+++ src/routes/account/chatbots/[id]/settings/+layout.svelte
@@ -3,8 +3,15 @@
 	import Chat from '$lib/components/Chat.svelte';
 	import { currentBot, state, alert } from '$lib/stores';
 	import { updateModel } from '$lib/models.js';
+	import { beforeNavigate, goto } from '$app/navigation';
+	import Modal from '$lib/components/Modal.svelte';
+
+
 
 	export let data;
+
+	$: console.log(data)
+
 
 	const links = [
 		{ name: 'Prompts', url: 'prompts' },
@@ -14,7 +21,42 @@
 		{ name: 'Delete', url: 'delete' }
 	];
 
+	let saved = true
+
 	$: currentPath = $page.url.pathname.split('/').pop();
+
+	const saveState = JSON.stringify($currentBot)
+
+
+	const checkIfSaved = () => {
+		if(saveState === JSON.stringify($currentBot)) {
+			saved = true
+		} else {
+			saved = false
+		}
+	}
+
+	$: $currentBot, checkIfSaved()
+
+	let nextURL: string;
+	let warningIgnored = false;
+
+	beforeNavigate(({to, cancel}) => {
+		if (!saved && !warningIgnored) {
+			cancel()
+			nextURL = to.url.pathname
+			confirmUnsavedNavigate.showModal()
+		}
+	})
+
+	const navigateWithoutSaving = () => {
+		warningIgnored = true;
+		currentBot.set(JSON.parse(saveState));
+		goto(nextURL);
+		warningIgnored = false;
+		saved = true
+
+	};
 
 	const handleSave = async () => {
 		$state = 'saving';
@@ -47,7 +89,12 @@
 				{/each}
 			</ul>
 			<button class="btn btn-outline btn-success my-4 w-full" type="submit" on:click={handleSave}
-				>{$state == 'saving' ? 'Saving' : 'Save'}</button
+				>
+				{#if $state == 'saving'}
+				<span class="loading loading-spinner loading-xs"></span>
+				{/if}
+				
+				{$state == 'saving' ? 'Saving' : 'Save'}</button
 			>
 		</div>
 	</div>
@@ -66,3 +113,23 @@
 		</div>
 	</div>
 </div>
+
+<Modal id="confirmUnsavedNavigate" title="You have unsaved changes">
+	
+	<svelte:fragment slot="actions">
+		<button class="btn btn-success btn-outline">Stay Here</button>
+		<button class="btn btn-error btn-outline" on:click={navigateWithoutSaving}>Discard Changes</button>
+	</svelte:fragment>
+</Modal>
+
+
+<!-- <dialog id="confirmUnsavedNavigate" class="modal">
+	<form method="dialog" class="modal-box">
+		<h3 class="font-bold text-lg mb-8">You have unsaved changes</h3>
+		<button class="btn btn-success btn-outline">Stay Here</button>
+		<button class="btn btn-error btn-outline" on:click={navigateWithoutSaving}>Don't Save</button>
+	</form>
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog> -->
