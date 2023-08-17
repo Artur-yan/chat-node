d963adb061a29250d1ea385a266ecd11bc7d45aa
redshiftstudio <accounts@rdshft.io>
2023-08-17 11:35:13 +0100

    restyle

--- src/lib/components/Code.svelte
+++ src/lib/components/Code.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
-    export let language = 'javascript';
     export let code: string
 
     import hljs from 'highlight.js';
 	import 'highlight.js/styles/github-dark.css';
 
-    import javascript from 'highlight.js/lib/languages/javascript';
 
 	import CopyButton from '$lib/components/CopyButton.svelte';
 
-    
-    if (language === 'javascript') {
-        hljs.registerLanguage('javascript', javascript);
-    }
 </script>
 
-<div class="relative">
-    <pre><code>{@html hljs.highlight(code, {language:'javascript'}).value}</code></pre>
-    <div class="absolute -right-2 -top-2">
+<div class="rounded-lg bg-neutral relative">
+    <pre class="overflow-x-auto p-4 lg:p-8"><code>{@html hljs.highlightAuto(code).value}</code></pre>
+    <div class="absolute right-1 top-1">
         <CopyButton textToCopy={code} />
     </div>
-
 </div>
