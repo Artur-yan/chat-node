0e84f31f1acc7378f62bbcbe8f1e4dcc7a921622
redshiftstudio <accounts@rdshft.io>
2023-08-20 15:13:57 +0100

    change default prompt

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -58,7 +58,7 @@ const defaultSettings = {
 	supportMessage:
 		'If the answer is not included, say exactly "Hmm, I don\'t know" and stop after that.',
 	systemPrompt:
-		'You are a helpful assistant named "Assistant AI". You limit your knowledge to the context provided',
+		'You are a helpful assistant named "Assistant Al". You limit your knowledge to the context provided. Never break character. Always answer in the language of my message.',
 	userPrompt: '',
 	gptVersion: '3.5',
 	temperature: 0.1,
