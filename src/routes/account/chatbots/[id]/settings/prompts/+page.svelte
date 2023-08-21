c0b06ef0ec8afa1a6ccb1b5543c3f79713fc513b
redshiftstudio <accounts@rdshft.io>
2023-08-21 12:13:34 +0100

    remove support prompt

--- src/routes/account/chatbots/[id]/settings/prompts/+page.svelte
+++ src/routes/account/chatbots/[id]/settings/prompts/+page.svelte
@@ -17,35 +17,6 @@
 		<input type="text" bind:value={$currentBot.settings.greeting} class="input w-full mb-2" />
 	</div>
 	<div>
-		<label for="support-message" class="label">
-			<span class="label-text"
-				>Support Prompt <span
-					class="tooltip tooltip-right badge"
-					data-tip="The support prompt is a message that we put at the end of your system prompt. It is usually an instruction to tell the assistant what to do if the question is outside the data scope or it just doesn't know the answer. You can change that to allow more or less flexibility in your chatbot knowledge domain answer."
-					>?</span
-				></span
-			>
-			<button
-				class="tooltip tooltip-left btn btn-xs btn-circle btn-ghost"
-				data-tip="Reset to default"
-				on:click|preventDefault={() =>
-					($currentBot.settings.supportMessage = defaultSettings.supportMessage)}
-				><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-					><path
-						fill="currentColor"
-						d="M20 13.5a6.5 6.5 0 0 1-6.5 6.5H6v-2h7.5c2.5 0 4.5-2 4.5-4.5S16 9 13.5 9H7.83l3.08 3.09L9.5 13.5L4 8l5.5-5.5l1.42 1.41L7.83 7h5.67a6.5 6.5 0 0 1 6.5 6.5Z"
-					/></svg
-				></button
-			>
-		</label>
-		<textarea
-			bind:value={$currentBot.settings.supportMessage}
-			class="textarea w-full leading-normal"
-			rows="2"
-			maxlength="800"
-		/>
-	</div>
-	<div>
 		<label for="system-prompt" class="label">
 			<span class="label-text"
 				>System Prompt <span
