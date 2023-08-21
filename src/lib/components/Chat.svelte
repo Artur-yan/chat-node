34e0283739692420097674b8066aa0443fc6e80c
redshiftstudio <accounts@rdshft.io>
2023-08-21 01:55:20 +0100

    make send button optional

--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -303,7 +303,7 @@
 					style="background-color: var(--inputBG); color: var(--inputText); border: 1px solid var(--inputBorder);"
 					{disabled}
 				/>
-				<!-- <button
+				<button
 					class="send-button btn btn-square btn-sm border-none rounded-lg join-item focus-within:outline-none absolute right-2 top-2"
 					type="submit"
 					name="Send"
@@ -319,7 +319,7 @@
 							d="M9.912 12H4L2.023 4.135A.662.662 0 0 1 2 3.995c-.022-.721.772-1.221 1.46-.891L22 12L3.46 20.896c-.68.327-1.464-.159-1.46-.867a.66.66 0 0 1 .033-.186L3.5 15"
 						/></svg
 					>
-				</button> -->
+				</button>
 			</div>
 		</div>
 	</form>
