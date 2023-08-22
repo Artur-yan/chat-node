a059f6b586fbdb8dc98ef3cefa88d9a788918f8a
redshiftstudio <accounts@rdshft.io>
2023-08-22 14:23:10 +0100

    add hxw to cover img

--- src/routes/(front)/blog/+page.svelte
+++ src/routes/(front)/blog/+page.svelte
@@ -4,8 +4,8 @@
 
 <div class="container grid grid-cols-3 gap-10">
 	{#each data.posts as post}
-		<a class="card bg-neutral overflow-hidden" href="/blog/{post.slug}">
-			<img src={post.thumbnail} />
+		<a class="card card-compact bg-neutral overflow-hidden" href="/blog/{post.slug}">
+			<img src={post.thumbnail} width="666" height="480" />
 			<div class="card-body">
 				<h2 class="card-title">{post.title}</h2>
 			</div>
