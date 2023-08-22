a62280d3e657d791b0383fcbec7d3022e84d2b6c
redshiftstudio <accounts@rdshft.io>
2023-08-22 17:00:10 +0100

    better error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.postcss'
+	import { page } from '$app/stores';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+
+</script>
+
+<Header />
+
+<section class="container text-center py-20 flex-1 flex items-center justify-center">
+
+	<h1 class="text-2xl">{$page.error.message}</h1>
+</section>
+
+<Footer />
--- src/routes/account/+error.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { page } from '$app/stores';
-</script>
-
-<section class="container text-center py-20">
-	<h1 class=" text-error font-bold">Error</h1>
-	<p>{$page.error.message}</p>
-</section>
