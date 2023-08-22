7ccad7d97e74e90c652f18cb1c1892eecf5c35fb
redshiftstudio <accounts@rdshft.io>
2023-08-22 23:38:18 +0100

    improve loading speed of blog add hover effects to posts

--- src/routes/(front)/blog/+page.server.ts
+++ src/routes/(front)/blog/+page.server.ts
@@ -2,6 +2,7 @@ const cmsPath = 'https://cms.chatnode.ai';
 const apiKey = 'API-0abe8486ae9683f1dfd7fe8565ee10cf82dddea2';
 
 export const load = async () => {
+
 	const data = await fetch(`${cmsPath}/api/content/items/post`, {
 		method: 'GET',
 		headers: {
@@ -10,23 +11,32 @@ export const load = async () => {
 	});
 	const posts = await data.json();
 
-	for (let i = 0; i < posts.length; i++) {
-		const res = await fetch(
-			`${cmsPath}/api/assets/image/${posts[i].img._id}?m=thumbnail&w=666&h=480`,
-			{
-				method: 'GET',
-				headers: {
-					'api-key': apiKey
+	const fetchPostImages = async () => {
+		let postImages = []
+		for (let i = 0; i < posts.length; i++) {
+			const res = await fetch(
+				`${cmsPath}/api/assets/image/${posts[i].img._id}?m=thumbnail&w=666&h=480`,
+				{
+					method: 'GET',
+					headers: {
+						'api-key': apiKey
+					}
 				}
-			}
-		);
+			);
+	
+			const img = await res.text();
+	
+			postImages.push({thumbnail: img});
+		}
 
-		const img = await res.text();
-
-		posts[i].thumbnail = img;
+		return postImages;
 	}
 
+
 	return {
-		posts
+		posts: posts,
+		streamed: {
+			postImages: fetchPostImages()
+		}
 	};
 };
--- src/routes/(front)/blog/+page.svelte
+++ src/routes/(front)/blog/+page.svelte
@@ -1,14 +1,34 @@
 <script>
 	export let data;
+
+	console.log(data);
+
+
+
 </script>
 
 <div class="container grid grid-cols-3 gap-10">
-	{#each data.posts as post}
-		<a class="card card-compact bg-neutral overflow-hidden" href="/blog/{post.slug}">
-			<img src={post.thumbnail} width="666" height="480" />
+	{#each data.posts as post, i}
+		<a class="card card-compact bg-neutral overflow-hidden group" href="/blog/{post.slug}">
+				{#await data.streamed.postImages }
+					<img src="/post-placeholder.png" width="666" height="480" alt={post.img.description} />
+				{:then postImages}
+
+					<img src={postImages[i].thumbnail} width="666" height="480" alt="" class="group-hover:scale-105 origin-bottom transition-transform" />
+
+				{/await}
 			<div class="card-body">
-				<h2 class="card-title">{post.title}</h2>
+				<div class="flex justify-between gap-2">
+					<div class="flex flex-1 overflow-x-auto gap-2">
+						{#each post.tags as tag}
+						<span class="badge badge-sm badge-outline badge-primary">{tag}</span>
+						{/each}
+					</div>
+					<span class="text-sm font-light">{new Date(post._modified * 1000).toLocaleDateString()}</span>
+				</div>
+				<h2 class="card-title group-hover:text-secondary transition-colors">{post.title}</h2>
 			</div>
 		</a>
 	{/each}
+
 </div>
