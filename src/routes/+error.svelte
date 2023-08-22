b8133768a205eff27850c70f6dfa1ff27991d0c5
redshiftstudio <accounts@rdshft.io>
2023-08-22 17:01:53 +0100

    change error msg style

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -10,7 +10,7 @@
 
 <section class="container text-center py-20 flex-1 flex items-center justify-center">
 
-	<h1 class="text-2xl">{$page.error.message}</h1>
+	<h1 class="text-3xl font-extralight">{$page.error.message}</h1>
 </section>
 
 <Footer />
