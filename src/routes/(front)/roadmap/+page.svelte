4c1662482e4065c4b9c1c183ec041e648cced7a3
redshiftstudio <accounts@rdshft.io>
2023-08-23 16:44:42 +0100

    make roadmap responsive

--- src/routes/(front)/roadmap/+page.svelte
+++ src/routes/(front)/roadmap/+page.svelte
@@ -5,15 +5,15 @@
 <div class="container">
 	<h1 class="text-2xl font-bold mb-4">Roadmap</h1>
 
-	<div class="grid grid-cols-3 gap-8">
+	<div class="grid md:grid-cols-3 gap-4 lg:gap-8">
 		{#each data.roadmap as item}
 			{#if !item._pid}
 				<div class="card bg-neutral">
-					<div class="card-body">
+					<div class="card-body p-4 text-sm lg:p-8">
 						<h2 class="card-title">{item.Title}</h2>
 						{#each data.roadmap as subItem}
 							{#if subItem._pid == item._id}
-								<h3 class="alert">{subItem.Title}</h3>
+								<h3 class="alert justify-start">{subItem.Title}</h3>
 							{/if}
 						{/each}
 					</div>
