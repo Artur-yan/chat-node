67307a729659c1c7f2bfdfe88ad5c2821e56e974
redshiftstudio <accounts@rdshft.io>
2023-08-27 14:52:18 +0100

    change cancel scripting to stop scraping

--- src/lib/components/AddModelData.svelte
+++ src/lib/components/AddModelData.svelte
@@ -93,6 +93,15 @@
 
 	const cancelFetchUrlsToScrape = () => {
 		busyFetchingUrls = false;
+		// urls = undefined
+		// selectedUrls = [];
+		// selectedUrlsTokenCount = 0;
+		// urlsTokenCount = 0
+
+
+	}
+	const clearFetchUrlsToScrape = () => {
+		busyFetchingUrls = false;
 		urls = undefined
 		selectedUrls = [];
 		selectedUrlsTokenCount = 0;
@@ -380,7 +389,7 @@
 			</table>
 			<button type="button" class="btn btn-warning btn-xs btn-outline mb-4" on:click={cancelFetchUrlsToScrape}>
 				<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12"><path fill="currentColor" d="M2.22 2.22a.749.749 0 0 1 1.06 0L6 4.939L8.72 2.22a.749.749 0 1 1 1.06 1.06L7.061 6L9.78 8.72a.749.749 0 1 1-1.06 1.06L6 7.061L3.28 9.78a.749.749 0 1 1-1.06-1.06L4.939 6L2.22 3.28a.749.749 0 0 1 0-1.06Z"/></svg>
-				<span>{!busyFetchingUrls && urls ? 'Clear URLs' : 'Cancel Scraping'}</span></button>
+				<span>{!busyFetchingUrls && urls ? 'Clear URLs' : 'Stop Scraping'}</span></button>
 			<div
 				class="alert mb-2 flex-col"
 				class:alert-warning={selectedUrlsTokenCount + existingTokenCount > subscription.max_tocken}
