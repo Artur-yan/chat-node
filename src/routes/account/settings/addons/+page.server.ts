760cdf9d4a070f93599ea5cc0280514d94924241
redshiftstudio <accounts@rdshft.io>
2023-08-29 18:51:19 +0100

    cleanup

--- src/routes/account/settings/addons/+page.server.ts
+++ src/routes/account/settings/addons/+page.server.ts
@@ -1,12 +1,8 @@
 import { prismaClient } from '$lib/server/prisma';
-import { fail } from '@sveltejs/kit';
-import tiersMap from '$lib/data/tiers.js';
 
 export const load = async ({ locals }) => {
 	const session = await locals.auth.validate();
 	if (!session) return;
-
-	console.log('session', session);
 
 	try {
 		return { session };
