08b49ffed10e8cbc220c9340be447cd6e152cd70
redshiftstudio <accounts@rdshft.io>
2023-08-31 16:16:26 +0100

    bigfix: quotes in quotes

--- src/routes/account/chatbots/[id]/settings/prompts/+page.svelte
+++ src/routes/account/chatbots/[id]/settings/prompts/+page.svelte
@@ -152,7 +152,7 @@
 			<span class="label-text">
 				Creativity <span
 					class="tooltip tooltip-right badge"
-					data-tip="The "creativity" in ChatGPT (known officially as "temperature") refers to the level of randomness in the generated responses. A higher temperature will result in more random and creative responses, while a lower temperature will produce more focused and deterministic responses."
+					data-tip='The "creativity" in ChatGPT (known officially as "temperature") refers to the level of randomness in the generated responses. A higher temperature will result in more random and creative responses, while a lower temperature will produce more focused and deterministic responses.'
 				>
 					?
 				</span>
