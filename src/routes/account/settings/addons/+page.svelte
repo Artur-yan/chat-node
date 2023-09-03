3c1ddf3e3024c4573c5345173732ed6f0de53dbe
redshiftstudio <accounts@rdshft.io>
2023-09-03 23:47:54 +0100

    new ui for bots addon

--- src/routes/account/settings/addons/+page.svelte
+++ src/routes/account/settings/addons/+page.svelte
@@ -10,14 +10,19 @@
 
 	console.log(addons);
 
-	let messagesToAdd = 0;
+	let messagesToAdd = 1;
 	let messages = 0
-	let botsToAdd = 0;
+	let botsToAdd = 1;
 	let bots = 0;
-	let addBranding = 0;
-	let branding = 0;
-	let tokensToAdd = 0;
+	let tokensToAdd = 1;
 	let tokens = 0;
+	let addBranding = 1;
+	let branding = 0;
+
+	let modifyMessages = false
+	let modifyBots = false
+	let modifyTokens = false
+	let modifyBranding = false
 
 	console.log(addons);
 
@@ -66,47 +71,60 @@
 					<span class="badge">$5/mo. each</span>
 				</h2>
 				<p>Add individual bots to your plan allowance.</p>
-				{#if bots !== 0}
-					<p class="badge badge-success badge-lg">Installed</p>
-				{/if}
-				<div class="flex mt-10 items-center gap-2">
-					<div class="flex gap-1 flex-1 items-center">
-						<button
-							class="btn btn-ghost btn-circle text-error"
-							on:click={() => botsToAdd--}
-							disabled={botsToAdd === 0}
-							aria-label="Remove Bot"
-						>
-							<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
-								<path
-									fill="currentColor"
-									d="M16 3C8.832 3 3 8.832 3 16s5.832 13 13 13s13-5.832 13-13S23.168 3 16 3zm0 2c6.087 0 11 4.913 11 11s-4.913 11-11 11S5 22.087 5 16S9.913 5 16 5zm-6 10v2h12v-2H10z"
-								/>
-							</svg>
-						</button>
-						<div>{botsToAdd}</div>
-						<button class="btn btn-ghost btn-circle text-primary" on:click={() => botsToAdd++} disabled={data.subscription?.plan === 0} aria-label="Add Bot">
-							<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
-								<path
-									fill="currentColor"
-									d="M16 3C8.832 3 3 8.832 3 16s5.832 13 13 13s13-5.832 13-13S23.168 3 16 3zm0 2c6.087 0 11 4.913 11 11s-4.913 11-11 11S5 22.087 5 16S9.913 5 16 5zm-1 5v5h-5v2h5v5h2v-5h5v-2h-5v-5h-2z"
-								/>
-							</svg>
-						</button>
+				{#if bots}
+					<div class="mt-4 items-center flex gap-4 justify-start">
+						<div class="badge badge-success badge-lg">Installed</div>
+						<div class="badge badge-lg">{bots}x </div>
+						<div class="badge badge-lg">${bots * 5}/mo.</div>
 					</div>
+					{/if}
+					
+					<div class="card-actions mt-10 items-center">
+						<button class="btn btn-primary btn-outline" on:click={() => modifyBots = !modifyBots} class:hidden={modifyBots}>Modify Plan</button>
+						{#if modifyBots}
+						<div class="flex join">
+							<div class="flex join-item items-center border border-primary">
+								<button
+									class="btn btn-ghost btn-square text-error"
+									on:click={() => botsToAdd--}
+									disabled={botsToAdd === 1}
+									aria-label="Remove Bot"
+								>
+									<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
+										<path
+											fill="currentColor"
+											d="M16 3C8.832 3 3 8.832 3 16s5.832 13 13 13s13-5.832 13-13S23.168 3 16 3zm0 2c6.087 0 11 4.913 11 11s-4.913 11-11 11S5 22.087 5 16S9.913 5 16 5zm-6 10v2h12v-2H10z"
+										/>
+									</svg>
+								</button>
+								<div class="badge badge-primary badge-lg mx-2">{botsToAdd}</div>
+								<button class="btn btn-ghost btn-square text-success" on:click={() => botsToAdd++} aria-label="Add Bot">
+									<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
+										<path
+											fill="currentColor"
+											d="M16 3C8.832 3 3 8.832 3 16s5.832 13 13 13s13-5.832 13-13S23.168 3 16 3zm0 2c6.087 0 11 4.913 11 11s-4.913 11-11 11S5 22.087 5 16S9.913 5 16 5zm-1 5v5h-5v2h5v5h2v-5h5v-2h-5v-5h-2z"
+										/>
+									</svg>
+								</button>
+								<button
+									class="join-item btn btn-primary"
+									on:click={() => handleCheckout(10002, botsToAdd)}
+									disabled={bots === botsToAdd}
+								>
+								<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 11l4-7m10 7l-4-7M2 11h20M3.5 11l1.6 7.4a2 2 0 0 0 2 1.6h9.8c.9 0 1.8-.7 2-1.6l1.7-7.4M9 11l1 9m-5.5-4.5h15M15 11l-1 9"/></svg>
+								</button>
+							</div>
+						</div>
 						<button
-							class="btn btn-primary btn-outline"
+							class="btn btn-error btn-xs btn-outline border-none"
 							on:click={() => handleCheckout(10002, botsToAdd)}
-							disabled={botsToAdd === bots}
 						>
-							{bots > 0 ? "Modify" : 'Purchase'} <span class="badge badge-sm" class:hidden={botsToAdd === 0}>
-								${botsToAdd * 7}
-							</span>
+						Remove
 						</button>
+						{/if}
+
+					</div>
 				</div>
-	
-				<div class="card-actions justify-end" />
-			</div>
 		</div>
 		<div class="card bg-neutral">
 			<div class="card-body">
