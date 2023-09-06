91723c868d9597282c306cbb1676b10a22215d26
redshiftstudio <accounts@rdshft.io>
2023-09-06 20:17:11 +0100

    block traffic to dev

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,20 @@
+import { PUBLIC_ENVIRONMENT } from '$env/static/public';
+
 import { auth } from '$lib/server/lucia';
 import { redirect, type Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.auth = auth.handleRequest(event);
+let i = 0
 
-	if (event.url.pathname.startsWith('/account')) {
-		if (!event.locals.auth) {
+
+export const handle: Handle = async ({ event, resolve }) => {
+	event.locals.auth = await auth.handleRequest(event);
+
+	const { user } = await event.locals.auth.validateUser();
+
+
+	if (PUBLIC_ENVIRONMENT !== 'production' && !user && event.url.pathname !== '/login') {
+		if(event.url.searchParams.get('pw') !== 'f8g4t9grn4t94gbv9g3bi34g9hg') {
+			console.log(event.url.searchParams.get('pw'))
 			throw redirect(303, '/login');
 		}
 	}
