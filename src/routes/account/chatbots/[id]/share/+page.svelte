9fa95fc56f124255989b70f4375f711a5e4ca800
redshiftstudio <accounts@rdshft.io>
2023-09-07 20:09:22 +0100

    add link to share settings

--- src/routes/account/chatbots/[id]/share/+page.svelte
+++ src/routes/account/chatbots/[id]/share/+page.svelte
@@ -26,7 +26,7 @@
 	<title>Embed | {data.model.name} | ChatNode</title>
 </svelte:head>
 
-<div class="container my-10">
+<div class="container my-4">
 	{#if data.model.settings.public == false}
 		<div class="alert alert-warning mb-4">
 			<svg
@@ -48,6 +48,25 @@
 			<div>
 				<a class="btn btn-sm" href="/account/chatbots/{data.model.id}/settings/sharing">Settings</a>
 			</div>
+		</div>
+	{:else}
+		<div class="alert mb-4">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="stroke-current shrink-0 w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+				/>
+			</svg>
+			<span>
+				Update your <a class="underline text-primary" href="settings/sharing">share settings</a> to change where your bot can be embedded.
+			</span>
 		</div>
 	{/if}
 
