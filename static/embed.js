84852a432fc365343bc74bf7edc32d66f7a96ae8
redshiftstudio <accounts@rdshft.io>
2023-09-07 17:33:03 +0100

    close quote bug

--- static/embed.js
+++ static/embed.js
@@ -32,7 +32,7 @@ const iframe = `
 const html = `	
 <div id="chatbot-${id}">
 <div id="chatbot-${id}-chat">
-<div class="close-btn role="button" onclick="toggleChat()" aria-label="Close AI Chat">
+<div class="close-btn" role="button" onclick="toggleChat()" aria-label="Close AI Chat">
 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m7 7l10 10M7 17L17 7"/></svg>
 </div>
 </div>
