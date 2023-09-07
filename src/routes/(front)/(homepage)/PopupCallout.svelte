94dd7df7b3abdffaf38778b0fda42a39a4433ad9
redshiftstudio <accounts@rdshft.io>
2023-09-07 21:52:46 +0100

    change height of popup callout

--- src/routes/(front)/(homepage)/PopupCallout.svelte
+++ src/routes/(front)/(homepage)/PopupCallout.svelte
@@ -1,5 +1,5 @@
 <div
-	class="fixed bg-secondary p-4 pl-6 pr-24 h-[6.5rem] items-center bottom-0 left-0 right-0 flex flex-wrap justify-between -z-10"
+	class="fixed bg-secondary p-4 pl-6 pr-24 h-[4.625rem] items-center bottom-0 left-0 right-0 flex flex-wrap justify-between -z-10"
 >
 	<div class="text-secondary-content font-bold text-lg">Still have questions?</div>
 	<div class="text-secondary-content text-lg">Ask our own chatbot &rarr;</div>
