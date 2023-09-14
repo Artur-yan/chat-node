256f87aba8036804c125694506bdef0418edb137
redshiftstudio <accounts@rdshft.io>
2023-09-14 19:14:46 +0200

    add retraining

--- src/routes/account/chatbots/[id]/data/+page.svelte
+++ src/routes/account/chatbots/[id]/data/+page.svelte
@@ -3,7 +3,6 @@
 	import { invalidateAll } from '$app/navigation';
 	import AddModelData from '$lib/components/AddModelData.svelte';
 	import Chat from '$lib/components/Chat.svelte';
-	import Icon from '@iconify/svelte';
 	import { alert } from '$lib/stores.js';
 
 	export let data;
@@ -41,6 +40,27 @@
 			invalidateAll();
 			restart();
 		}
+	};
+
+	const retrainUrls = async (s3_keys: Array<string>) => {
+		trainingStatus = 'training';
+		let body = new FormData();
+		body.append('user_id', data.model.user_id);
+		body.append('session_id', data.user.session.sessionId);
+		body.append('chat_key', data.model.id);
+		body.append('source_keys', s3_keys);
+
+		const res = await fetch(PUBLIC_CHAT_API_URL + '/api/retrain-urls', {
+			method: 'POST',
+			body
+		});
+		const resData = await res.json();
+
+		if (res.ok) {
+			invalidateAll();
+			restart();
+		}
+
 	};
 </script>
 
@@ -98,8 +118,20 @@
 											deleteDataSourceModal.showModal();
 										}}
 									>
-										<Icon icon="mdi:delete-outline" width="16" />
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12M8 9h8v10H8V9m7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5Z"/></svg>
 									</button>
+								</td>
+								<td>
+									{#if modelData.source_type === 'url'}
+									<button
+										class="btn btn-sm btn-circle btn-ghost"
+										on:click={() => {
+											retrainUrls([modelData.s3_key]);
+										}}
+									>
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M15 12c0-1.7-1.3-3-3-3s-3 1.3-3 3s1.3 3 3 3s3-1.3 3-3zm2-8.7C13.1 1.1 8.3 1.8 5.1 4.7V3c0-.6-.4-1-1-1s-1 .4-1 1v4.5c0 .6.4 1 1 1h4.5c.6 0 1-.4 1-1s-.4-1-1-1H6.2C7.7 4.9 9.8 4 12 4c4.4 0 8 3.6 8 8c0 .6.4 1 1 1s1-.4 1-1c0-3.6-1.9-6.9-5-8.7zm2.9 12.2h-4.5c-.6 0-1 .4-1 1s.4 1 1 1h2.4C16.3 19.1 14.2 20 12 20c-4.4 0-8-3.6-8-8c0-.6-.4-1-1-1s-1 .4-1 1c0 5.5 4.5 10 10 10c2.6 0 5-1 6.9-2.8V21c0 .6.4 1 1 1s1-.4 1-1v-4.5c0-.6-.5-1-1-1z"/></svg>
+									</button>
+									{/if}
 								</td>
 							</tr>
 						{/each}
