a259480247d03c2e5493373b5f01d92f94fba67d
redshiftstudio <accounts@rdshft.io>
2023-09-22 14:08:46 +0200

    add fbevents text page

---
+++ src/routes/(test)/fbevents/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { onMount } from "svelte";
+
+    onMount(() => {
+		fbq('track', 'PageView');
+    })
+</script>
+
+<svelte:head>
+    <script>
+		fbq('track', 'PageView');
+    </script>
+</svelte:head>
