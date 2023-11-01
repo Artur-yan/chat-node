6279d61f81c19bbe53f857ee2ae6ffc6f596b676
redshiftstudio <accounts@rdshft.io>
2023-11-01 19:52:09 +0000

    round chat window

--- src/routes/account/chatbots/[id]/+page.svelte
+++ src/routes/account/chatbots/[id]/+page.svelte
@@ -7,15 +7,15 @@
 	<title>{data.model.name} | ChatNode</title>
 </svelte:head>
 
-<div class="flex flex-col flex-1 p-10">
-	<Chat
-		modelId={data.model.id}
-		trainingStatus={data.model.status}
-		settings={data.model.settings}
-		avatar={data.model?.avatar_img}
-		userId={data.user.user.userId}
-	/>
-</div>
+	<div class="flex flex-col flex-1 overflow-hidden rounded-lg m-2 md:m-8">
+		<Chat
+			modelId={data.model.id}
+			trainingStatus={data.model.status}
+			settings={data.model.settings}
+			avatar={data.model?.avatar_img}
+			userId={data.user.user.userId}
+		/>
+	</div>
 
 <div>
 	<style>
