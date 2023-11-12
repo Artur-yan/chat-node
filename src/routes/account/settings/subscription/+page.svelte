7468e41470c66eaa87e83047a518478080356eac
redshiftstudio <accounts@rdshft.io>
2023-11-12 16:54:45 +0000

    allow tier 5 people to upgrade

--- src/routes/account/settings/subscription/+page.svelte
+++ src/routes/account/settings/subscription/+page.svelte
@@ -65,7 +65,7 @@
 </svelte:head>
 
 <div class="container mb-20">
-	{#if (currentPlan > 1000 && currentPlan < 1005) || currentPlan === 0 && $page.url.search.includes('agency-c7433')}
+	{#if (currentPlan > 1000 && currentPlan < 1006) || currentPlan === 0 && $page.url.search.includes('agency-c7433')}
 		<div class="grid md:grid-cols-[4fr_3fr] my-8 gap-8">
 			<div>
 				<h1 class="font-bold text-3xl mb-4">
