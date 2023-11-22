81420db4678a558b80d4947b2cd59c614e71c288
redshiftstudio <accounts@rdshft.io>
2023-11-22 17:06:13 +0000

    test fix for invalidatesesison error

--- src/routes/(front)/(auth)/logout/+server.ts
+++ src/routes/(front)/(auth)/logout/+server.ts
@@ -6,14 +6,13 @@ export const POST: RequestHandler = async ({ locals }) => {
 	const session = await locals.auth.validate();
 	if (!session) {
 		throw redirect(302, '/');
+	} else {
+		try {
+			await auth.invalidateSession(session.sessionId);
+			locals.auth.setSession(null);
+			throw redirect(302, '/');
+		} catch (e) {
+			console.error(e);
+		}
 	}
-
-	try {
-		await auth.invalidateSession(session.sessionId);
-		locals.auth.setSession(null);
-	} catch (e) {
-		console.error(e);
-	}
-
-	throw redirect(302, '/');
 };
