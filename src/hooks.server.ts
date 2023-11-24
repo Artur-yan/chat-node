d83939acf3ea2b2b9a1d7b5168a3ac8953bfa347
redshiftstudio <accounts@rdshft.io>
2023-11-24 11:00:25 +0000

    remove acces restriction for testing

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,11 +9,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
 	const session = await event.locals.auth.validate();
 
-	if (PUBLIC_ENVIRONMENT !== 'production' && !session && event.url.pathname !== '/login') {
-		if (event.url.searchParams.get('pw') !== 'f8g4t9grn4t94gbv9g3bi34g9hg') {
-			throw redirect(303, '/login');
-		}
-	}
+	// if (PUBLIC_ENVIRONMENT !== 'production' && !session && event.url.pathname !== '/login') {
+	// 	if (event.url.searchParams.get('pw') !== 'f8g4t9grn4t94gbv9g3bi34g9hg') {
+	// 		throw redirect(303, '/login');
+	// 	}
+	// }
 
 	return await resolve(event);
 };
