1a884fa7e20fd3daf7e6f0119af5748f4281265f
Hunter Stevens <hunter@toplobster.io>
2023-12-01 12:51:49 -0500

    Added more model options to UI

--- src/routes/account/chatbots/[id]/settings/gpt/versionLabel.svelte
+++ src/routes/account/chatbots/[id]/settings/gpt/versionLabel.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
   export let title: string;
   export let value: string;
-  export let currentBot;
+  export let currentBot: any;
+  export let disable: boolean;
 </script>
 
-<label class="btn join-item btn-outline btn-neutral aria-checked:bg-primary flex justify-start">
+<label class="pl-[7px] pr-0 btn join-item btn-outline btn-neutral aria-checked:bg-primary flex flex-wrap justify-start" class:btn-disabled={disable}>
   <input
       class="radio radio-sm radio-primary"
       type="radio"
       value={value}
       bind:group={$currentBot.settings.gptVersion}
+      disabled={disable}
   />
-  {title}
+    {title}
 </label>
