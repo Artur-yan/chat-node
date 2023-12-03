7e79a4d6765657141b29ceea538c4bbd7aca3feb
Sacha Dumay <dumaysacha@gmail.com>
2023-12-03 23:17:59 +0000

    adding default openAI key for everyone

--- src/routes/account/settings/+page.svelte
+++ src/routes/account/settings/+page.svelte
@@ -154,7 +154,7 @@
 				{/if}
 			</div>
 		</div>
-		{#if data.subscription.plan !== 0}
+		{#if ![0, 1, 101].includes(data.subscription.plan)}
 			<form class="card bg-neutral">
 				<div class="card-body">
 					<h2 class="card-title">Default OpenAI API Key</h2>
