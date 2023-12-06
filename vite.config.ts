88635834c7e49975fdf7b2d3329391bd4d7d99ab
Sacha Dumay <dumaysacha@gmail.com>
2023-12-06 12:01:18 +0000

    handle robot.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { imagetools } from '@zerodevx/svelte-img/vite';
+import fs from 'fs';
+
+// Function to handle the robots.txt file
+function handleRobotsFile() {
+	const env = process.env.VITE_ENV || 'Development';
+	const sourceFile = `static/robots.${env}.txt`;
+	const destinationFile = 'static/robots.txt';
+
+	fs.copyFileSync(sourceFile, destinationFile);
+}
 
 export default defineConfig({
-	plugins: [sveltekit(), imagetools()]
+	plugins: [sveltekit(), imagetools()],
+	define: () => {
+    handleRobotsFile();
+  }
 });
