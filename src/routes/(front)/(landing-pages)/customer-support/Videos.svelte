a3acd03fc32ad077cfd38cc87330e60024112bdf
Sacha Dumay <dumaysacha@gmail.com>
2023-12-08 15:03:09 +0000

    add video clicking

--- src/routes/(front)/(landing-pages)/customer-support/Videos.svelte
+++ src/routes/(front)/(landing-pages)/customer-support/Videos.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import { onMount } from "svelte";
 	let video: HTMLVideoElement;
 
-	onMount(() => {
+	const loadVideo = () => {
 		video.preload = "auto";
-	});
+	}
 </script>
 
-<section id="demo"
+<section on:mouseover={loadVideo} id="demo"
     class="flex items-center justify-center md:p-14 from-black via-accent to-base-300 bg-gradient-to-br"
 >
     <div class="w-full max-w-5xl">
