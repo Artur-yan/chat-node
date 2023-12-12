64037b301c83f3982ce6fc7536d28655e35c7584
Hunter Stevens <hunter@toplobster.io>
2023-12-12 16:53:04 -0500

    Changed clipboard icon and added message

--- src/routes/account/chatbots/[id]/settings/sharing/+page.svelte
+++ src/routes/account/chatbots/[id]/settings/sharing/+page.svelte
@@ -4,6 +4,7 @@
 	import { onMount } from 'svelte';
 
 	export let data;
+	let message:string = '';
 
 	const addUrl = (url: string) => {
 		$currentBot.settings.allowedUrls = [...$currentBot.settings.allowedUrls, url];
@@ -51,6 +52,14 @@
 		}
 		return { warning: false };
 	};
+
+	const copyIdToClipboard = () => {
+		navigator.clipboard.writeText($currentBot.id);
+		message = 'Copied!';
+		setTimeout(() => {
+			message = '';
+		}, 2000);
+	};
 </script>
 
 <div class="card bg-neutral card-compact mb-4">
@@ -84,12 +93,24 @@
 		<div class="card-title">
 			<h2>Bot ID</h2>
 		</div>
-		<input
-		class="input w-full"
-		type="text"
-		bind:value={$currentBot.id}
-		readonly
-	/>
+		<div class="flex gap-2 items-center">
+			<input
+			class="input"
+			type="text"
+			bind:value={$currentBot.id}
+			readonly
+		/>
+		<button 
+			class="btn btn-neutral btn-sm"
+			on:click={copyIdToClipboard}
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+			</svg>	
+				
+		</button>
+		{message}
+		</div>
 	</div>
 </div>
 
