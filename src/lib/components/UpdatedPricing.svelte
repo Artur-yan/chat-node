ac2872747efd06103efc588089112782d4887de5
Hunter Stevens <hunter@toplobster.io>
2023-12-18 23:35:32 -0500

    Inital design for new pricing table

---
+++ src/lib/components/UpdatedPricing.svelte
@@ -0,0 +1,204 @@
+<div id="pricing" class="isolate overflow-hidden bg-gray-900">
+  <div class="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
+    <div class="mx-auto max-w-4xl">
+      <h2 class="text-base font-semibold leading-7 text-indigo-400">Plans</h2>
+      <h1 class="p-2 text-4xl font-bold tracking-tight sm:text-6xl bg-gradient-to-r from-yellow-300 to-orange-600 text-transparent bg-clip-text">Affordable Pricing</h1>
+    </div>
+    <div class="relative mt-6">
+      <p class="mx-auto max-w-2xl text-lg leading-8 text-white/60">Flexible Pricing for Every Business Size</p>
+      <svg viewBox="0 0 1208 1024" class="absolute -top-16 left-1/2 -z-10 h-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]">
+        <ellipse cx="604" cy="512" fill="url(#6d1bd035-0dd1-437e-93fa-59d316231eb0)" rx="604" ry="512" />
+        <defs>
+          <radialGradient id="6d1bd035-0dd1-437e-93fa-59d316231eb0">
+            <stop stop-color="#FE4981" />
+            <stop offset="1" stop-color="#5256FD" />
+          </radialGradient>
+        </defs>
+      </svg>
+    </div>
+  </div>
+  <div class="flow-root pb-24 sm:pb-32">
+    <div class="-mt-80">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto grid max-w-lg grid-cols-1 lg:grid-cols-3 gap-8 lg:max-w-5xl">
+          <div class="flex flex-col justify-between rounded-3x p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10 rounded-3xl bg-white bg-opacity-25">
+            <div>
+              <h3 class="text-4xl font-semibold leading-7 text-indigo-300">Standard</h3>
+              <div class="mt-4 flex items-baseline gap-x-2">
+                <span class="text-3xl font-bold tracking-tight text-gray-100">$50</span>
+                <span class="text-base font-semibold leading-7 text-gray-100">/ Month</span>
+              </div>
+              <p class="mt-6 text-base leading-7 text-gray-300">14-days free trial</p>
+              <ul class="mt-10 space-y-4 text-sm leading-6 text-gray-600">
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  5000 messages credit
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  5 chatbots
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Unlimited data sources
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Embed on unlimited websites
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Slack, Crisp, Typebot Integrations
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Chatbot history
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Capture leads
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Customise chatbot design
+                </li>
+              </ul>
+            </div>
+            <a href="/contact">
+              <button class="flex w-full justify-center mt-10 bg-black bg-opacity-20 px-3.5 py-2.5 text-md font-semibold rounded-md border-2 border-purple-400">
+                <span class="mx-1 bg-gradient-to-r from-orange-300 to-orange-500 text-transparent bg-clip-text">Try for free!</span>
+              </button>   
+            </a>       
+          </div>
+          <div class="flex flex-col justify-between rounded-3xl bg-white bg-opacity-25 p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10">
+            <div>
+              <h3 id="tier-team" class="text-4xl font-semibold leading-7 text-indigo-300">Pro</h3>
+              
+              <div class="mt-4 flex items-baseline gap-x-2">
+                <span class="text-3xl font-bold tracking-tight text-gray-100">$83</span>
+                <span class="text-base font-semibold leading-7 text-gray-100">/ Month</span>
+              </div>
+              <p class="mt-6 text-base leading-7 text-gray-300">14-days free trial</p>
+              <ul class="mt-10 space-y-4 text-sm leading-6 text-gray-100">
+                <li class="flex gap-x-3">
+                  <svg class="h-6 w-5 flex-none text-indigo-200" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Everything on Standard plan
+                </li>
+                <li class="flex gap-x-3">
+                  <svg class="h-6 w-5 flex-none text-indigo-200" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  10,000 messages credit
+                </li>
+                <li class="flex gap-x-3">
+                  <svg class="h-6 w-5 flex-none text-indigo-200" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Unlimited messages with OpenAI key
+                </li>
+                <li class="flex gap-x-3">
+                  <svg class="h-6 w-5 flex-none text-indigo-200" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  No chatnode branding
+                </li>
+                <li class="flex gap-x-3">
+                  <svg class="h-6 w-5 flex-none text-indigo-200" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  API access
+                </li>
+              </ul>
+            </div>
+            <a href="/contact">
+              <button class="flex w-full justify-center mt-10 bg-black bg-opacity-20 px-3.5 py-2.5 text-md font-semibold rounded-md border-2 border-purple-400">
+                <span class="mx-1 bg-gradient-to-r from-orange-300 to-orange-500 text-transparent bg-clip-text">Try for free!</span>
+              </button>   
+            </a>        
+          </div>
+
+          <div class="flex flex-col justify-between rounded-3x p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10 rounded-3xl bg-white bg-opacity-25">
+            <div>
+              <h3 class="text-4xl font-semibold leading-7 text-indigo-300">Custom</h3>
+              <div class="mt-4 flex items-baseline gap-x-2">
+                <span class="text-3xl font-bold tracking-tight text-gray-100">Contact Us</span>
+                <span class="text-base font-semibold leading-7 text-gray-100 hidden"></span>
+              </div>
+              <p class="mt-6 text-base leading-7 text-gray-300 invisible">14-days free trial</p>
+              <ul class="mt-10 space-y-4 text-sm leading-6 text-gray-600">
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Everything in Pro plan
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  Personalized 1:1 onboarding
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  We set up your chatbots
+                </li>
+                <li class="flex gap-x-3 text-white">
+                  <svg class="h-6 w-5 flex-none text-indigo-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                  </svg>
+                  VIP support (Dedicated Key Account Manager)
+                </li>
+              </ul>
+            </div>
+            <a href="/contact">
+              <button class="flex w-full justify-center mt-10 bg-black bg-opacity-20 px-3.5 py-2.5 text-md font-semibold rounded-md border-2 border-purple-400">
+                <span class="mx-1 bg-gradient-to-r from-orange-300 to-orange-500 text-transparent bg-clip-text">Reach Out!</span>
+              </button>   
+            </a>       
+          </div>
+
+
+          <!-- Add ons -->
+          <div class="flex flex-col items-start gap-x-8 gap-y-6  bg-white bg-opacity-25 rounded-3xl p-8 ring-1 ring-gray-900/10 sm:gap-y-10 sm:p-10 lg:col-span-3 lg:flex-row lg:items-center">
+            <h1 class="p-2 text-2xl font-bold tracking-tight bg-gradient-to-r from-black via-stone-800 to-black text-transparent bg-clip-text">Add-ons</h1>
+            <div class="mx-auto max-w-4xl">
+              <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
+                <div>
+                  Extra Message Credit
+                </div>
+
+                <div>
+                  Extra Chatbot
+                </div>
+
+                <div>
+                  Custom Domain
+                </div>
+              </div>
+            </div> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
