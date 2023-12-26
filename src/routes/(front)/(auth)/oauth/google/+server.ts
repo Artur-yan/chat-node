c7603e362cc5d49d46904148691413553e3b4e5a
Hunter Stevens <hunter@toplobster.io>
2023-12-26 11:59:09 -0500

    Force state in URL string

--- src/routes/(front)/(auth)/oauth/google/+server.ts
+++ src/routes/(front)/(auth)/oauth/google/+server.ts
@@ -4,25 +4,25 @@ import { PUBLIC_SITE_URL } from '$env/static/public';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 
 export const GET = async ({ url }) => {
-	const plan = url.searchParams.get('plan');
+	const plan = url.searchParams.get('plan') || '-1';
 	const configs = {
 		clientId: GOOGLE_CLIENT_ID,
 		clientSecret: GOOGLE_CLIENT_SECRET,
 		redirectUri: `${PUBLIC_SITE_URL}/oauth/redirect-google`,
-		scope: ['email'],
-		state: plan
+		scope: ['email']
 	};
 
 	const googleAuth = google(auth, configs);
 
 	const googleUrl = await googleAuth.getAuthorizationUrl();
 	const urlString = googleUrl[0].href;
-	console.log('urlString', urlString);
+	const newUrl = new URL(urlString);
+	newUrl.searchParams.set('state', plan);
 
 	return new Response(null, {
 		status: 302,
 		headers: {
-			location: urlString
+			location: newUrl.toString()
 		}
 	});
 };
