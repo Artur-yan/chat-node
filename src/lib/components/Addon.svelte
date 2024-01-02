6cabe906536f8c19dc06e93cfe6426e11553cd3c
Hunter Stevens <hunter@toplobster.io>
2024-01-02 13:52:16 -0500

    Update color shades

--- src/lib/components/Addon.svelte
+++ src/lib/components/Addon.svelte
@@ -86,7 +86,7 @@
 							<ol role="list" class="flex items-center space-x-4">
 								<li>
 									<div>
-										<span class="ml-1 text-xs font-medium text-gray-400 hover:text-gray-700">Dashboard</span>
+										<span class="ml-1 text-xs font-medium text-gray-400 hover:text-gray-300">Dashboard</span>
 									</div>
 								</li>
 								<li>
@@ -94,7 +94,7 @@
 										<svg class="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 											<path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
 										</svg>
-										<span class="ml-1 text-xs font-medium text-gray-400 hover:text-gray-700">Chatbot</span>
+										<span class="ml-1 text-xs font-medium text-gray-400 hover:text-gray-300">Chatbot</span>
 									</div>
 								</li>
 								<li>
@@ -102,7 +102,7 @@
 										<svg class="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 											<path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
 										</svg>
-										<span class="ml-1 text-xs font-medium text-gray-400 hover:text-gray-700" aria-current="page">Share</span>
+										<span class="ml-1 text-xs font-medium text-gray-400 hover:text-gray-300" aria-current="page">Share</span>
 									</div>
 								</li>
 							</ol>
