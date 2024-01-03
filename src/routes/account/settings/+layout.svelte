42ed4995d3d5ea82480b8dd789fe5955214cb79d
Hunter Stevens <hunter@toplobster.io>
2024-01-03 17:21:58 -0500

    Displaying trail expiration in subscription

--- src/routes/account/settings/+layout.svelte
+++ src/routes/account/settings/+layout.svelte
@@ -5,6 +5,7 @@
 	import { redirectToStripeBilling } from '$lib/account';
 
 	export let data;
+	const isTrialing = data.subscription?.status === 'trialing';
 
 	const subscriptionPath = '/account/settings/subscription';
 	const settingsPath = '/account/settings';
@@ -37,7 +38,19 @@
 		</div>
 	</div>
 </div>
-{#if data.subscription?.next_billing_cycle}
+{#if isTrialing}
+	<!-- <div class="container mt-4">
+		<div class="badge badge-warning bg-white bg-opacity-50 p-4 font-semibold text-lg text-yellow-500">
+			Trial ends in 
+			<span class="text-yellow-500 mx-1"> {daysLeftInBillingCycle} </span> 
+			days
+		</div>
+	</div> -->
+	<div role="alert" class="md:w-1/4 mx-auto mt-4 alert alert-warning">
+		<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+		<span class="font-bold tex-lg">Trail ends in {daysLeftInBillingCycle} days</span>
+	</div>
+{:else if data.subscription?.next_billing_cycle}
 	<div class="container mt-4">
 		<div class="card card-compact card-bordered border-neutral mb-4">
 			<div class="card-body">
