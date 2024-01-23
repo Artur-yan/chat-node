58cb819dc9f2229f8a51aa7ef885ae605924a96e
Sacha Dumay <dumaysacha@gmail.com>
2024-01-23 17:17:23 +0100

    fix proxy ? é

--- src/routes/layout.svelte
+++ src/routes/layout.svelte
@@ -2,7 +2,7 @@
   import { onMount } from 'svelte';
   import { page } from '$app/stores';
   import { trackPageView } from '$lib/tracking';
-  import { PUBLIC_PLAUSIBLE_DOMAIN } from '$env/static/public';
+  import { PUBLIC_PLAUSIBLE_DOMAIN, PUBLIC_SITE_URL } from "$env/static/public";
   onMount(() => {
     trackPageView();
     
@@ -14,5 +14,6 @@
 </script>
 
 <svelte:head>
-  {@html `<script defer data-domain="${PUBLIC_PLAUSIBLE_DOMAIN}" src="https://plausible.io/js/script.tagged-events.js"></script>`}
+  <!--{@html `<script defer data-domain="${PUBLIC_PLAUSIBLE_DOMAIN}" src="https://plausible.io/js/script.tagged-events.js"></script>`}-->
+  {@html `<script  src="/stats/js/script.js" data-api="/stats/api/event" data-domain="${PUBLIC_PLAUSIBLE_DOMAIN}"></script>`}
 </svelte:head>
