d1ead4fb09414b8278d5b33be768c204ac1438c9
Sacha Dumay <dumaysacha@gmail.com>
2024-02-26 13:33:21 +0000

    add carbon

--- src/routes/api/data-sources/scraping/website/+server.ts
+++ src/routes/api/data-sources/scraping/website/+server.ts
@@ -1,16 +1,43 @@
 import { CB_TOKEN } from '$env/static/private';
 import { json } from "@sveltejs/kit";
 
+
+interface ScrapeConfig {
+  urls: string[];
+  recursionDepth: number;
+  maxPagesToScrape: number;
+  chunkSize: number;
+  chunkOverlap: number;
+  skipEmbeddingGeneration: boolean;
+  enableAutoSync: boolean;
+  embeddingModel: string;
+}
+
+interface ScrapeConfigSingleUrl extends Omit<ScrapeConfig, 'urls'> {
+  url: string;
+}
+
+function transformConfigToSingleUrlList(config: ScrapeConfig): ScrapeConfigSingleUrl[] {
+  return config.urls.map(url => ({
+    url,
+    recursionDepth: config.recursionDepth,
+    maxPagesToScrape: config.maxPagesToScrape,
+    chunkSize: config.chunkSize,
+    chunkOverlap: config.chunkOverlap,
+    skipEmbeddingGeneration: config.skipEmbeddingGeneration,
+    enableAutoSync: config.enableAutoSync,
+    embeddingModel: config.embeddingModel,
+  }));
+}
+
+
 export const POST = async ({ request, locals }) => {
   const {
     bot_id, urls, recursionDepth, maxPagesToScrape, chunkSize,
     chunkOverlap, enableAutoSync
   } = await request.json();
 
-  const options = {
-    method: 'POST',
-    headers: { authorization: `Bearer ${CB_TOKEN}`, 'Content-Type': 'application/json', 'customer-id': bot_id },
-    body: JSON.stringify({
+  const listOfConfigsWithSingleUrl = transformConfigToSingleUrlList({
       urls: urls,
       recursionDepth: recursionDepth,
       maxPagesToScrape: maxPagesToScrape,
@@ -19,7 +46,13 @@ export const POST = async ({ request, locals }) => {
       skipEmbeddingGeneration: false,
       enableAutoSync: enableAutoSync,
       embeddingModel: 'OPENAI_ADA_LARGE_3072'
-    })
+    });
+
+
+  const options = {
+    method: 'POST',
+    headers: { authorization: `Bearer ${CB_TOKEN}`, 'Content-Type': 'application/json', 'customer-id': bot_id },
+    body: JSON.stringify(listOfConfigsWithSingleUrl)
   };
 
   const response = await fetch('https://api.carbon.ai/web_scrape', options)
