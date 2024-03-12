2313073c7243c559684047f67030254745ae56f3
Sacha Dumay <dumaysacha@gmail.com>
2024-03-12 15:18:50 +0100

    limit to zero

--- src/lib/components/data/WebScraping.svelte
+++ src/lib/components/data/WebScraping.svelte
@@ -240,8 +240,7 @@
 
       if (response.status === 200) {
         // Assuming data?.urls is an array of strings containing URLs
-        const filteredUrls = data?.urls.filter(url => !url.endsWith('.jpg') && !url.endsWith('.png'));
-
+        const filteredUrls = data?.urls.filter(url => !url.endsWith('.jpg') && !url.endsWith('.png') && !url.endsWith('.jpeg'));
         // Now, filteredUrls will contain all URLs except those ending with .jpg or .png
 
         const webScrapingResponse = await submitWebScraping(filteredUrls, 0, baseSitemapOrigin)
