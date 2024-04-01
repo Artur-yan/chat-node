60016ff86d7e0821943297231fc86970478e3273
Hunter Stevens <hunter@toplobster.io>
2024-04-01 16:48:47 -0500

    Added animation when connecting

--- src/lib/components/data/Notion.svelte
+++ src/lib/components/data/Notion.svelte
@@ -7,6 +7,7 @@
 	let isModalOpen = false;
   let activeTab: 'upload' | 'trained' = 'upload';
   let isUploading = false;
+  let isRedirecting = false;
   let hasQueuedFiles = false;
   let counter: number;
   let intervalId: any;
@@ -51,8 +52,10 @@
   }
 
   async function connectToNotion() {
+    isRedirecting = true;
     const data = await fetchOAuthUrl();
     const notionUrl = data.oauth_url;
+    isRedirecting = false;
     window.open(notionUrl, '_blank');
   }
 
@@ -116,11 +119,15 @@
     <h3 class="text-2xl text-secondary font-semibold whitespace-nowrap leading-none tracking-tight">Connect Your Notion Account</h3>
     <div class="py-2">
       <button 
-        class="inline-flex gap-2 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-black hover:bg-primary/90 h-10 px-4 py-2"
+        class="inline-flex gap-2 w-28 min-w-28 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-black hover:bg-primary/90 h-10 px-4 py-2"
         on:click={connectToNotion}
-        >
+      >
+      {#if isRedirecting}
+        <span class="loading loading-spinner loading-md"></span>
+      {:else }
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cable"><path d="M4 9a2 2 0 0 1-2-2V5h6v2a2 2 0 0 1-2 2Z"/><path d="M3 5V3"/><path d="M7 5V3"/><path d="M19 15V6.5a3.5 3.5 0 0 0-7 0v11a3.5 3.5 0 0 1-7 0V9"/><path d="M17 21v-2"/><path d="M21 21v-2"/><path d="M22 19h-6v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2Z"/></svg>
         Connect
+      {/if}
     </button>
   </div>
 </div>
