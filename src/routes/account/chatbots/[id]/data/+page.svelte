51067025cf1e41caadb51f585546152b11fb26b9
Sacha Dumay <dumaysacha@gmail.com>
2024-04-04 14:19:26 +0900

    migration 2021-09-14-01-00-00 for chatnode-3 pod

--- src/routes/account/chatbots/[id]/data/+page.svelte
+++ src/routes/account/chatbots/[id]/data/+page.svelte
@@ -150,7 +150,7 @@
 	}
 
 	onMount(() => {
-		if(!$currentBot.settings.dataFunnelV2 && $currentBot.pinecone_info.PINECONE_INDEX === '') {
+		if(!$currentBot.settings.dataFunnelV2 && $currentBot.pinecone_info.PINECONE_INDEX === 'chatnode-3') {
 			my_modal_1.showModal();
 		}
 
