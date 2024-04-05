bd6078906634ada9cc43f660320690d529ea55cf
Hunter Stevens <hunter@toplobster.io>
2024-04-05 16:34:28 -0500

    New close SVG can update colors

--- src/routes/account/chatbots/[id]/settings/customize/+page.svelte
+++ src/routes/account/chatbots/[id]/settings/customize/+page.svelte
@@ -670,7 +670,7 @@
 					<!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="{$currentBot?.settings.theme.popupButtonIcon}" class="w-8 h-8">
 						<path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
 					</svg> -->
-					<svg width="24" height="24" viewBox="0 0 24 24" fill="${$currentBot?.settings.theme.popupButtonBG}" xmlns="http://www.w3.org/2000/svg" stroke="${$currentBot?.settings.theme.popupButtonIcon}"  stroke-width="0.5"><path fill-rule="evenodd" clip-rule="evenodd" d="M18.601 8.39897C18.269 8.06702 17.7309 8.06702 17.3989 8.39897L12 13.7979L6.60099 8.39897C6.26904 8.06702 5.73086 8.06702 5.39891 8.39897C5.06696 8.73091 5.06696 9.2691 5.39891 9.60105L11.3989 15.601C11.7309 15.933 12.269 15.933 12.601 15.601L18.601 9.60105C18.9329 9.2691 18.9329 8.73091 18.601 8.39897Z" fill="${$currentBot.settings.theme.popupButtonIcon}"></path></svg>
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="{$currentBot?.settings.theme.popupButtonBG}" xmlns="http://www.w3.org/2000/svg" stroke="{$currentBot.settings.theme.popupButtonIcon}"  stroke-width="0.5"><path fill-rule="evenodd" clip-rule="evenodd" d="M18.601 8.39897C18.269 8.06702 17.7309 8.06702 17.3989 8.39897L12 13.7979L6.60099 8.39897C6.26904 8.06702 5.73086 8.06702 5.39891 8.39897C5.06696 8.73091 5.06696 9.2691 5.39891 9.60105L11.3989 15.601C11.7309 15.933 12.269 15.933 12.601 15.601L18.601 9.60105C18.9329 9.2691 18.9329 8.73091 18.601 8.39897Z" fill="{$currentBot.settings.theme.popupButtonIcon}"></path></svg>
 
 				</div>
 				<ColorPicker
