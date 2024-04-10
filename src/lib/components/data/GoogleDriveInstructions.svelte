64704fd70f6805bbc4dcf9de36e5bc57b092cd5d
Hunter Stevens <hunter@toplobster.io>
2024-04-10 12:36:41 -0500

    Added new required fields to generate OAuth URL and added instructions

---
+++ src/lib/components/data/GoogleDriveInstructions.svelte
@@ -0,0 +1,96 @@
+<div class="flow-root my-4 ml-8">
+  <ul class="-mb-8">
+    <li>
+      <div class="relative pb-8">
+        <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-primary" aria-hidden="true"></span>
+        <div class="relative flex space-x-3">
+          <div>
+            <span class="h-8 w-8 rounded-full bg-secondary flex items-center justify-center">
+              1
+            </span>
+          </div>
+          <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+            <div>
+              <p class="text-lg text-gray-300 ml-4 mb-4">Select your account</p>
+              <img class="w-1/2 rounded-lg" src="/google-drive-step-1.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="relative pb-8">
+        <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-primary" aria-hidden="true"></span>
+        <div class="relative flex space-x-3">
+          <div>
+            <span class="h-8 w-8 rounded-full bg-secondary flex items-center justify-center">
+              2
+            </span>
+          </div>
+          <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+            <div>
+              <p class="text-lg text-gray-300 ml-4 mb-4">Sign in to your account</p>
+              <img class="w-1/2 rounded-lg" src="/google-drive-step-2.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="relative pb-8">
+        <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-primary" aria-hidden="true"></span>
+        <div class="relative flex space-x-3">
+          <div>
+            <span class="h-8 w-8 rounded-full bg-secondary flex items-center justify-center">
+              3
+            </span>
+          </div>
+          <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+            <div>
+              <p class="text-lg text-gray-300 ml-4 mb-4">Give ChatNode access to your Google Account</p>
+              <img class="w-1/2 rounded-lg" src="/google-drive-step-3.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <li>
+        <div class="relative pb-8">
+          <span class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-primary" aria-hidden="true"></span>
+          <div class="relative flex space-x-3">
+            <div>
+              <span class="h-8 w-8 rounded-full bg-secondary flex items-center justify-center">
+                4
+              </span>
+            </div>
+            <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+              <div>
+                <p class="text-lg text-gray-300 ml-4 mb-4">Click "Select Files from Google"</p>
+                <img class="w-1/2 rounded-lg" src="/google-drive-step-3.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li>
+      <div class="relative pb-8">
+        <div class="relative flex space-x-3">
+          <div>
+            <span class="h-8 w-8 rounded-full bg-secondary flex items-center justify-center">
+              5
+            </span>
+          </div>
+          <div class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+            <div>
+              <p class="text-lg text-gray-300 ml-4 mb-4">Click "Select Files from Google"</p>
+              <img class="w-1/2 rounded-lg" src="/google-drive-step-4.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+</div>
+
+<br><br><br><br>
